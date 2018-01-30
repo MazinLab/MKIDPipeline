e124b2bee0c76b5a357213e925e22a75aa60d34a
Neelay Fruitwala <neelay@dark.physics.ucsb.edu>
2018-01-29 18:47:48 -0800

    Fixed bugs in ObsFile header defs and consolidatePhotonTables

--- RawDataProcessing/consolidatePhotonTables.py
+++ RawDataProcessing/consolidatePhotonTables.py
@@ -14,12 +14,15 @@ nRows = 0
 for pixTable in hfile.iter_nodes('/Photons'):
     nRows += pixTable.shape[0]
 
-photonTable = hfile.create_table('/Photons', 'PhotonTable', ObsFileCols, 'Photon Table', expectedrows=nRows)
+print(ObsFileCols)
+photonTable = hfile.create_table('/Photons', 'PhotonTable', ObsFileCols, 'Photon Table', expectedrows=nRows, chunkshape=300)
 
 beamMap = hfile.get_node('/BeamMap/Map').read()
 resIDList = np.sort(beamMap.flatten())
 
 for resID in resIDList:
+    if resID==2**32-1:
+        continue
     pixelTable = hfile.get_node('/Photons/' + str(resID)).read()
     photonTable.append(pixelTable)
     photonTable.flush()
