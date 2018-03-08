e878016ad7f8f8f55977d1f28067021b83bac855
IsabelLipartito <isabel.lipartito@gmail.com>
2018-03-08 10:58:56 -0800

    Revised 'applyflatcal' function

--- RawDataProcessing/darkObsFile.py
+++ RawDataProcessing/darkObsFile.py
@@ -834,13 +834,6 @@ class ObsFile:
                             the noise tail) during the effective exposure.
         """
 
-<<<<<<< Updated upstream
-=======
-        wvlStart=wvlStart if (wvlStart!=None and wvlStart>0.) else (self.wvlLowerLimit if (self.wvlLowerLimit!=None and self.wvlLowerLimit>0.) else 700)
-        wvlStop=wvlStop if (wvlStop!=None and wvlStop>0.) else (self.wvlUpperLimit if (self.wvlUpperLimit!=None and self.wvlUpperLimit>0.) else 1500)
-
-        print(xCoord,yCoord) 
->>>>>>> Stashed changes
         photonList = self.getPixelPhotonList(xCoord, yCoord, firstSec, integrationTime)
         return self._makePixelSpectrum(photonList, applySpecWeight=applySpecWeight,
                                   applyTPFWeight=applyTPFWeight, wvlStart=wvlStart, wvlStop=wvlStop,
