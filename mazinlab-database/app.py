80a0a8bac128a49aa69ababfdd5e2bb580303ff5
clarissardoo <40551328+clarissardoo@users.noreply.github.com>
2018-07-23 10:05:49 -0700

    targets working.

--- mazinlab-database/app.py
+++ mazinlab-database/app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
 def homepage():
@@ -10,20 +11,59 @@ def homepage():
 	#return send_file(os.path.join(app.config['/'], 'database.html'))
 
 
-@app.route('/finder')
-def finder():
-	filelist = ('2016B', '2017A', '2017B', '2018A')
-	data = []
-	for recordYear in filelist:
-		with open(recordYear+'J.json') as datarunner:
-				data1 = json.load(datarunner)
-		data.append(data1)	
-	return(json.dumps(data[0]))
-	#return redirect(url_for('meme')
-	#return(json.dumps(data1[0]))
-	#print(data1)
-	#return('\n'.join('{}{}'.format(key, val) for key, val in data1[3].items()))
+@app.route('/targetfinder', methods=['POST'])
+def targetfinder():
+	if request.method == 'POST':
+		filelist = ('2016B', '2017A', '2017B', '2018A')
+		data = []
+		for recordYear in filelist:
+			with open(recordYear+'J.json') as datarunner:
+					data1 = json.load(datarunner)
+			for elem in data1:
+				if elem.get("Target") == request.form['submit']:
+					data.append(elem)	
+		return jsonify(data)
 
 
+
+@app.route('/runfinder', methods=['POST'])
+def runfinder():
+	if request.method == 'POST':
+		filelist = ('2016B', '2017A', '2017B', '2018A')
+		data = []
+		for recordYear in filelist:
+			with open(recordYear+'J.json') as datarunner:
+					data1 = json.load(datarunner)
+			for elem in data1:
+				if elem.get("Sunset Date(s)") == request.form['submit']:
+					data.append(elem)	
+		return jsonify(data)
+
+@app.route('/calibrationfinder', methods=['POST'])
+def calibrationfinder():
+	if request.method == 'POST':
+		filelist = ('Wavecals_2017A', 'WaveCals_2017B', 'WaveCals_2018A')
+		data = []
+		for recordYear in filelist:
+			with open(recordYear+'.json') as datarunner:
+					data1 = json.load(datarunner)
+			for elem in data1:
+				if elem.get("Target") == request.form['submit']:
+					data.append(elem)	
+		return jsonify(data)
+
+
+@app.route('/datefinder', methods=['POST'])
+def datefinder():
+	if request.method == 'POST':
+		filelist = ('2016B', '2017A', '2017B', '2018A')
+		data = []
+		for recordYear in filelist:
+			with open(recordYear+'J.json') as datarunner:
+					data1 = json.load(datarunner)
+			for elem in data1:
+				if elem.get("Sunset Date(s)") == request.form['submit']:
+					data.append(elem)	
+		return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 3000)
