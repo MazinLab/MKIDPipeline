009f9d7f2a0e5e9670c4118079a5d9b49e44be5a
Jeb Bailey <baileyji@umich.edu>
2018-07-26 10:31:34 -0700

    tabs to spaces

--- mkidpipeline/clarity/app.py
+++ mkidpipeline/clarity/app.py
@@ -5,66 +5,72 @@ import json
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+
 @app.route('/')
 def homepage():
-	return render_template('database.html')
-	#return send_file(os.path.join(app.config['/'], 'database.html'))
+    return render_template('database.html')
+
+
+# return send_file(os.path.join(app.config['/'], 'database.html'))
 
 
 @app.route('/targetfinder', methods=['POST'])
 def targetfinder():
-	if request.method == 'POST':
-		filelist = ('2016B', '2017A', '2017B', '2018A')
-		data = []
-		for recordYear in filelist:
-			with open(recordYear+'J.json') as datarunner:
-					data1 = json.load(datarunner)
-			for elem in data1:
-				if elem.get("Target") == request.form['submit']:
-					data.append(elem)		
-		return render_template("targets.html", data = json.dumps(data))
+    if request.method == 'POST':
+        filelist = ('2016B', '2017A', '2017B', '2018A')
+        data = []
+        for recordYear in filelist:
+            with open(recordYear + 'J.json') as datarunner:
+                data1 = json.load(datarunner)
+            for elem in data1:
+                if elem.get("Target") == request.form['submit']:
+                    data.append(elem)
+        return render_template("targets.html", data=json.dumps(data))
 
 
 @app.route('/runfinder', methods=['POST'])
 def runfinder():
-	if request.method == 'POST':
-		filelist = ('2016B', '2017A', '2017B', '2018A')
-		data = []
-		for recordYear in filelist:
-			with open(recordYear+'J.json') as datarunner:
-					data1 = json.load(datarunner)
-			for elem in data1:
-				rundate = request.form['submit']
-				if rundate in elem.get("Sunset Date(s)"):
-					data.append(elem)	
-		return render_template("runs.html", data = json.dumps(data))
+    if request.method == 'POST':
+        filelist = ('2016B', '2017A', '2017B', '2018A')
+        data = []
+        for recordYear in filelist:
+            with open(recordYear + 'J.json') as datarunner:
+                data1 = json.load(datarunner)
+            for elem in data1:
+                rundate = request.form['submit']
+                if rundate in elem.get("Sunset Date(s)"):
+                    data.append(elem)
+        return render_template("runs.html", data=json.dumps(data))
+
 
 @app.route('/calibrationfinder', methods=['POST'])
 def calibrationfinder():
-	if request.method == 'POST':
-		filelist = ('Wavecals_2017A', 'WaveCals_2017B', 'WaveCals_2018A')
-		data = []
-		for recordYear in filelist:
-			with open(recordYear+'.json') as datarunner:
-					data1 = json.load(datarunner)
-			for elem in data1:
-				if elem.get("Target") == request.form['submit']:
-					data.append(elem)	
-		return render_template("cals.html", data = json.dumps(data))
+    if request.method == 'POST':
+        filelist = ('Wavecals_2017A', 'WaveCals_2017B', 'WaveCals_2018A')
+        data = []
+        for recordYear in filelist:
+            with open(recordYear + '.json') as datarunner:
+                data1 = json.load(datarunner)
+            for elem in data1:
+                if elem.get("Target") == request.form['submit']:
+                    data.append(elem)
+        return render_template("cals.html", data=json.dumps(data))
 
 
 @app.route('/datefinder', methods=['POST'])
 def datefinder():
-	if request.method == 'POST':
-		filelist = ('2016B', '2017A', '2017B', '2018A')
-		data = []
-		for recordYear in filelist:
-			with open(recordYear+'J.json') as datarunner:
-					data1 = json.load(datarunner)
-			for elem in data1:
-				date = request.form['submit']
-				if date in elem.get("Sunset Date(s)"):
-					data.append(elem)	
-		return render_template("dates.html", data = json.dumps(data))
+    if request.method == 'POST':
+        filelist = ('2016B', '2017A', '2017B', '2018A')
+        data = []
+        for recordYear in filelist:
+            with open(recordYear + 'J.json') as datarunner:
+                data1 = json.load(datarunner)
+            for elem in data1:
+                date = request.form['submit']
+                if date in elem.get("Sunset Date(s)"):
+                    data.append(elem)
+        return render_template("dates.html", data=json.dumps(data))
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port = 3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
