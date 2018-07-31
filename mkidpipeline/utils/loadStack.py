0191c80036d07a5318a1ccec59bd3a65e7082f50
Isabel Lipartito <ilipartito@umail.ucsb.edu>
2018-07-31 10:06:22 -0700

    Restructured code to have a separate cfg class and cal class

--- mkidpipeline/utils/loadStack.py
+++ mkidpipeline/utils/loadStack.py
@@ -10,107 +10,74 @@ from mkidpipeline.utils.parsePacketDump import parsePacketData
 Utilities for loading sets of image stacks from either .IMG or .bin files
 """
 
+
 def loadIMGStack(dataDir, start, stop, nCols=80, nRows=125, verbose=True):
     useImg = True
-    frameTimes = np.arange(start, stop+1)
+    frameTimes = np.arange(start, stop + 1)
     frames = []
-    for iTs,ts in enumerate(frameTimes):
+    for iTs, ts in enumerate(frameTimes):
         try:
-            if useImg==False:
-                imagePath = os.path.join(dataDir,str(ts)+'.bin')
+            if useImg == False:
+                imagePath = os.path.join(dataDir, str(ts) + '.bin')
                 print(imagePath)
-                with open(imagePath,'rb') as dumpFile:
+                with open(imagePath, 'rb') as dumpFile:
                     data = dumpFile.read()
 
                 nBytes = len(data)
-                nWords = nBytes/8 #64 bit words
+                nWords = nBytes / 8  # 64 bit words
 
-                #break into 64 bit words
-                words = np.array(struct.unpack('>{:d}Q'.format(nWords), data),dtype=object)
-                parseDict = parsePacketData(words,verbose=False)
+                # break into 64 bit words
+                words = np.array(struct.unpack('>{:d}Q'.format(nWords), data), dtype=object)
+                parseDict = parsePacketData(words, verbose=False)
                 image = parseDict['image']
 
             else:
-                imagePath = os.path.join(dataDir,str(ts)+'.img')
+                imagePath = os.path.join(dataDir, str(ts) + '.img')
                 if verbose:
                     print(imagePath)
-                image = np.fromfile(open(imagePath, mode='rb'),dtype=np.uint16)
+                image = np.fromfile(open(imagePath, mode='rb'), dtype=np.uint16)
                 image = np.transpose(np.reshape(image, (nCols, nRows)))
 
         except (IOError, ValueError):
             print("Failed to load ", imagePath)
-            image = np.zeros((nRows, nCols),dtype=np.uint16)
+            image = np.zeros((nRows, nCols), dtype=np.uint16)
         frames.append(image)
     stack = np.array(frames)
     return stack
-"""
+
 def loadBINStack(dataDir, start, stop, nCols=80, nRows=125):
     useImg = False
-    frameTimes = np.arange(start, stop+1)
+    frameTimes = np.arange(start, stop + 1)
     frames = []
-    for iTs,ts in enumerate(frameTimes):
-        try:
-            if useImg==False:
-                imagePath = os.path.join(dataDir,str(ts)+'.bin')
-                print(imagePath)
-                with open(imagePath,'rb') as dumpFile:
-                    data = dumpFile.read()
+    for iTs, ts in enumerate(frameTimes):
+        if useImg == False:
+            imagePath = os.path.join(dataDir, str(ts) + '.bin')
+            print(imagePath)
+            with open(imagePath, 'rb') as dumpFile:
+                data = dumpFile.read()
 
-                nBytes = len(data)
-                nWords = nBytes/8 #64 bit words
+            nBytes = len(data)
+            nWords = nBytes / 8  # 64 bit words
 
-                #break into 64 bit words
-                words = np.array(struct.unpack('>{:d}Q'.format(nWords), data),dtype=object)
-                parseDict = parsePacketData(words,verbose=False)
-                image = parseDict['image']
+            # break into 64 bit words
+            words = np.array(struct.unpack('>{:d}Q'.format(nWords), data), type=object)
+            parseDict = parsePacketData(words, verbose=False)
+            image = parseDict['image']
 
-            else:
-                imagePath = os.path.join(dataDir,str(ts)+'.img')
-                print(imagePath)
-                image = np.fromfile(open(imagePath, mode='rb'),dtype=np.uint16)
-                image = np.transpose(np.reshape(image, (nCols, nRows)))
+        else:
+            imagePath = os.path.join(dataDir, str(ts) + '.img')
+            print(imagePath)
+            image = np.fromfile(open(imagePath, mode='rb'), type=np.uint16)
+            image = np.transpose(np.reshape(image, (nCols, nRows)))
 
-        except (IOError, ValueError):
-            print("Failed to load ", imagePath)
-            image = np.zeros((nRows, nCols),dtype=np.uint16)
         frames.append(image)
-    stack = np.array(frames)
-    return stack
-"""
-def loadBINStack(dataDir, start, stop, nCols=80, nRows=125):
-    useImg = False
-    frameTimes = np.arange(start, stop+1)
-    frames = []
-    for iTs,ts in enumerate(frameTimes):
-            if useImg==False:
-                imagePath = os.path.join(dataDir,str(ts)+'.bin')
-                print(imagePath)
-                with open(imagePath,'rb') as dumpFile:
-                    data = dumpFile.read()
-
-                nBytes = len(data)
-                nWords = nBytes/8 #64 bit words
-
-                #break into 64 bit words
-                words = np.array(struct.unpack('>{:d}Q'.format(nWords), data),type=object)
-                parseDict = parsePacketData(words,verbose=False)
-                image = parseDict['image']
-
-            else:
-                imagePath = os.path.join(dataDir,str(ts)+'.img')
-                print(imagePath)
-                image = np.fromfile(open(imagePath, mode='rb'),type=np.uint16)
-                image = np.transpose(np.reshape(image, (nCols, nRows)))
-
-        
-            frames.append(image)
     stack = np.array(frames)
     return stack
 
 
 def loadH5Stack(h5Path, verbose=True):
     if (not os.path.exists(h5Path)):
-        msg='file does not exist: %s'%h5Path
+        msg = 'file does not exist: %s' % h5Path
         if verbose:
             print(msg)
         raise Exception(msg)
@@ -118,7 +85,7 @@ def loadH5Stack(h5Path, verbose=True):
 
     stackParams = f.get_node('/imageStack/params').read()
 
-    imageStack = np.array(f.get_node('/imageStack/rawImgs').read(),dtype=float)
+    imageStack = np.array(f.get_node('/imageStack/rawImgs').read(), dtype=float)
     timeStamps = np.array(f.get_node('/imageStack/timestamps').read())
     ditherArray = np.array(f.get_node('/imageStack/dithers').read())
     hotArray = np.array(f.get_node('/imageStack/hpms').read())
@@ -136,11 +103,11 @@ def loadH5Stack(h5Path, verbose=True):
     final = np.array(f.get_node('/imageStack/finalImg').read())
 
     f.close()
-    return {"stack":imageStack,"times":timeStamps,"params":stackParams,"final":final,"centX":centXArray,
-            "centY":centYArray,"dark":dark,"hpm":hotArray}
+    return {"stack": imageStack, "times": timeStamps, "params": stackParams, "final": final, "centX": centXArray,
+            "centY": centYArray, "dark": dark, "hpm": hotArray}
 
 
-def loadCubeStack(npzPath,verbose=True):
+def loadCubeStack(npzPath, verbose=True):
     """
     Load up times, cubes, and wvlBinEdges from npz output of makeCubeTimestream
     """
@@ -149,4 +116,4 @@ def loadCubeStack(npzPath,verbose=True):
     cubes = npzfile['cubes']
     wbe = npzfile['wvlBinEdges']
     npzfile.close()
-    return {"times":times,"cubes":cubes,"wvlBinEdges":wbe}
+    return {"times": times, "cubes": cubes, "wvlBinEdges": wbe}
