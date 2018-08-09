5c66e7a979dea7e3ffefa544d1c09e5bf658cf76
Clint Bockstiegel <clint@physics.ucsb.edu>
2018-08-09 10:40:36 -0700

    maximum likelihood estimator using scipy.optimize.minimize is working

--- mkidpipeline/speckle/binned_rician.py
+++ mkidpipeline/speckle/binned_rician.py
@@ -207,9 +207,9 @@ def binMRlogL(n, Ic, Is):
     its intensity distribution follows a blurred modified Rician with Ic, Is. 
     
     INPUTS:
-        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/sec]. Bin size must be fixed. 
-        Ic: Coherent portion of MR [1/second]
-        Is: Speckle portion of MR [1/second]
+        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/bin]. Bin size must be fixed. 
+        Ic: Coherent portion of MR [cts/bin]
+        Is: Speckle portion of MR [cts/bin]
     OUTPUTS:
         [float] the Log likelihood. 
     
@@ -217,6 +217,10 @@ def binMRlogL(n, Ic, Is):
     N = len(n)
     lnL = np.zeros(N) 
     tmp = np.zeros(N)
+    
+    if 1./(Is+1)<=0 or Is/(1.+Is) <=0:
+        lnL = np.nan
+        return lnL
     
     #hyp1f1 can't do numpy arrays because of its data type, which is mpf
 
@@ -249,9 +253,9 @@ def binMRlogL(n, Ic, Is):
 
 
 # v4. eval_laguerre can accept numpy arrays! Another factor of ~9 in speed. 
-    k = Ic/(Is**2 + Is)
-    tmp = np.log(eval_laguerre(n,-k))
-    tmp += k
+    k = -Ic/(Is**2 + Is)
+    tmp = np.log(eval_laguerre(n,k))
+    tmp -= k
     lnL = N*(np.log(1./(Is+1))  - 1.*Ic/Is) + np.sum(tmp) + np.sum(n)*np.log(Is/(1.+Is))
     
     return lnL
@@ -272,10 +276,19 @@ def negLogLike(p,n):
         negative log likelihood [float]
     """
     return -binMRlogL(n, p[0], p[1])
+
+
+def nLogLikeJac(p,n):
+    
+    return -binMRlogL_jacobian(n,p[0], p[1])
+    
+    
+def nLogLikeHess(p,n):
         
+    return -binMRlogL_hessian(n,p[0], p[1])
 
 
-def maxBinMRlogL(n, Ic_guess=1., Is_guess=1., method='Powell'):
+def maxBinMRlogL(n, Ic_guess=1., Is_guess=1., method='Newton-CG'):  # Newton-CG
     """
     Find the maximum likelihood values for Ic and Is for a given lightcurve.
     
@@ -289,20 +302,20 @@ def maxBinMRlogL(n, Ic_guess=1., Is_guess=1., method='Powell'):
     """
     p0 = np.array([Ic_guess,Is_guess])
     
-    t1 = time.time()
+#    t1 = time.time()
     
-    res = optimize.minimize(negLogLike, p0,n,bounds=((0.1,np.inf),(0.1,np.inf)))
+#    res = optimize.minimize(negLogLike, p0,n,bounds=((0.1,np.inf),(0.1,np.inf)))
     
-    t2 = time.time()
-    
-    dT = t2 - t1
-    
-    print('\nelapsed time for estimating Ic & Is by finding the maximum likelihood: ', dT, 'sec\n')
+    res = optimize.minimize(negLogLike, p0,n, method=method,jac = nLogLikeJac, hess=nLogLikeHess )
+
+#    t2 = time.time()
+#    dT = t2 - t1
+#    print('\nelapsed time for estimating Ic & Is by finding the maximum likelihood: ', dT, 'sec\n')
     
     Ic = res.x[0]/effExpTime
     Is = res.x[1]/effExpTime
     
-    print('\nIc,Is = ', Ic, Is)
+#    print('\nIc,Is = ', Ic, Is)
     
     return Ic,Is,res
 
@@ -315,12 +328,29 @@ def binMRlogL_jacobian(n,Ic,Is):
     The Jacobian is a vector of the first derivatives. 
     
     INPUTS:
-        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/sec]. Bin size must be fixed. 
-        Ic:
-        Is:
+        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/bin]. Bin size must be fixed. 
+        Ic: counts/bin
+        Is: counts/bin
     OUTPUTS:
         jacobian vector [dlnL/dIc, dlnL/dIs] at Ic, Is
     """
+    N = len(n)
+    k = -Ic/(Is**2 + Is)
+    tmp1 = eval_genlaguerre(n-1,1,k)
+    tmp2 = eval_laguerre(n,k)
+    tmp4 = tmp2*Is*(1+Is)
+    jac_Ic = -N/(1+Is) + np.sum(tmp1/tmp4)
+    
+    tmp3 = 1/(Is + Is**2)
+    jac_Is = N*(-1+Ic-Is)/(1+Is)**2 + np.sum(tmp3*n + k*tmp3*(1+2*Is)*tmp1/tmp2 ) 
+    
+#   check that my simplifications are correct. Compare the results to the raw output 
+#   from mathematica. 
+#    x = -N/Is + N/(Is + Is**2) + np.sum(eval_genlaguerre(n-1,1,k)/((Is + Is**2)*eval_laguerre(n,k)))
+#    y = N*(-1 + Ic - Is)/(1 + Is)**2 + np.sum( (1+Is)*(-Is/(1 + Is)**2 + 1/(1+Is) )/Is*n - 
+#           Ic*(1 + 2*Is)/(Is + Is**2)**2*eval_genlaguerre(n-1,1,k)/eval_laguerre(n,k))
+
+    return np.asarray([jac_Ic, jac_Is])
     
     
     
@@ -331,13 +361,61 @@ def binMRlogL_hessian(n,Ic,Is):
     The Hessian is a matrix of the second derivatives. 
     
     INPUTS:
-        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/sec]. Bin size must be fixed. 
-        Ic:
-        Is:
+        n: 1d array containing the (binned) intensity as a function of time, i.e. a lightcurve [counts/bin]. Bin size must be fixed. 
+        Ic: counts/bin
+        Is: counts/bin
     OUTPUTS:
         Hessian matrix [[d2lnL/dIc2, d2lnL/dIcdIs], [d2lnL/dIsdIc, d2lnL/dIsdIs]] at Ic, Is
     """
     
+    N = len(n)
+    tmp1 = 1/(Is + Is**2)
+    tmp2 = 1/(1+Is)
+    k = -Ic*tmp1
+    
+    
+#    H_IcIc = np.sum(eval_genlaguerre(n-2,2,k)/eval_laguerre(n,k) - eval_genlaguerre(n-1,1,k)**2/eval_laguerre(n,k)**2)*tmp1**2
+#    
+#    H_IcIs = N/(1+Is)**2 + np.sum( - Ic*(1+2*Is)*eval_genlaguerre(n-2,2,k)*tmp1*tmp1*tmp1/eval_laguerre(n,k) - (1+2*Is)*eval_genlaguerre(n-1,1,k)*tmp1*tmp1/eval_laguerre(n,k) + Ic*(1+2*Is)*eval_genlaguerre(n-1,1,k)**2*tmp1*tmp1*tmp1/eval_laguerre(n,k)**2 )
+#    
+#    H_IsIs = N*(1-2*Ic+Is)*tmp2**3 + np.sum( -(1+2*Is)*n/(Is**2)*tmp2**2 + Ic**2*(1+2*Is)**2*eval_genlaguerre(n-2,2,k)*tmp1**4/eval_laguerre(n,k) + 2*Ic*(1+2*Is)**2*eval_genlaguerre(n-1,1,k)*tmp1**3/eval_laguerre(n,k) -  2*Ic*eval_genlaguerre(n-1,1,k)*tmp1**2/eval_laguerre(n,k) - Ic**2*(1+2*Is)**2*eval_genlaguerre(n-1,1,k)**2*tmp1**4/eval_laguerre(n,k)**2 )
+#    
+#    Hessian = np.asarray([[H_IcIc, H_IcIs],[H_IcIs, H_IsIs]])
+    
+    
+    
+    
+    N = len(n)
+    a = 1/(1+Is)                  # a = 1/(1 + Is)
+    b = a/Is                      # b = 1/(Is + Is**2)
+    c = 1 + 2*Is                  # c = 1 + 2*Is
+    d = b*b                       # d = 1/(Is+ Is**2)**2
+    e = d*b                       # e = 1/(Is+ Is**2)**3
+    f = e*b                       # f = 1/(Is+ Is**2)**4
+    g = Ic*c                      # g = Ic*(1 + 2*Is)
+    h = Ic**2                     # h = Ic**2
+    
+    k = -Ic*b                     # k = -Ic/(Is + Is**2)   last argument of every laguerre call
+    l = 1/eval_laguerre(n,k)      # l = 1/eval_laguerre(n,k)
+    m = l*l                       # m = 1/eval_laguerre(n,k)**2
+    q = eval_genlaguerre(n-1,1,k) # q = eval_genlaguerre(n-1,1,k)
+    o = q*q                       # o = eval_genlaguerre(n-1,1,k)**2
+    p = eval_genlaguerre(n-2,2,k) # p = eval_genlaguerre(n-2,2,k)
+
+    
+    h_IcIc = np.sum(p*d*l - o*d*m)
+    
+    h_IcIs = N*a**2 + np.sum( -g*p*e*l - c*q*d*l + g*o*e*m )
+    
+    h_IsIs = N*(1-2*Ic+Is)*a**3 + np.sum( -c*n/Is**2*a**2  + h*c**2*p*f*l + 2*Ic*c**2*q*e*l - 2*Ic*q*d*l - h*c**2*o*f*m  )
+    
+    hessian = np.asarray([[h_IcIc, h_IcIs],[h_IcIs, h_IsIs]])
+    
+    
+
+    return hessian
+
+
 
 
 
@@ -358,19 +436,6 @@ def plotLogLMap(n, Ic_list, Is_list, effExpTime,IcPlusIs = False):
     Is_list_countsperbin = Is_list*effExpTime
     
     im = np.zeros((len(Ic_list),len(Is_list)))
-    
-#    for i, Ic in enumerate(Ic_list_countsperbin): #calculate maximum likelihood for a grid of 
-#        for j, Is in enumerate(Is_list_countsperbin):   #Ic,Is values using counts/bin
-#            print('Ic,Is = ',Ic/effExpTime,Is/effExpTime)
-#            if IcPlusIs==True:
-#                tmp = Ic - Is
-#                else:
-#                tmp = Ic
-#            
-#            lnL = binMRlogL(n, tmp, Is)
-#            im[i,j] = lnL
-            
-            
             
     for j, Is in enumerate(Is_list_countsperbin):
         for i, Ic in enumerate(Ic_list_countsperbin):
@@ -382,25 +447,14 @@ def plotLogLMap(n, Ic_list, Is_list, effExpTime,IcPlusIs = False):
             lnL = binMRlogL(n, tmp, Is)
             im[i,j] = lnL
         print('Ic,Is = ',Ic/effExpTime,Is/effExpTime)
-            
-        
-        
-            
+       
     Ic_ind, Is_ind=np.unravel_index(im.argmax(), im.shape)
     print('Max at ('+str(Ic_ind)+', '+str(Is_ind)+')')
     print("Ic="+str(Ic_list[Ic_ind])+", Is="+str(Is_list[Is_ind]))
     print(im[Ic_ind, Is_ind])
 
     
-#    l_90 = np.percentile(im, 90)
-#    l_max=np.amax(im)
-#    l_min=np.amin(im)
-#    levels=np.linspace(l_90,l_max,int(len(im.flatten())*.1))
-    
     plt.figure()
-#    plt.contourf(Ic_list, Is_list,im.T,levels=levels,extend='min')
-  
-    
     X, Y = np.meshgrid(Ic_list, Is_list)
     sigmaLevels = np.array([8.36, 4.78, 2.1])
     levels = np.amax(im) - sigmaLevels
@@ -409,16 +463,24 @@ def plotLogLMap(n, Ic_list, Is_list, effExpTime,IcPlusIs = False):
     oldstyle = {key:matplotlib.rcParams[key] for key in MYSTYLE}
     matplotlib.rcParams.update(MYSTYLE)
 
-#    plt.contourf(X,Y,im.T)
     plt.imshow(im.T,extent = [np.amin(Ic_list), np.amax(Ic_list), np.amin(Is_list), np.amax(Is_list)],aspect='auto',origin = 'lower')
-    plt.contour(X,Y,im.T,colors='black',levels = levels)
+    
+    CS = plt.contour(X,Y,im.T,colors='black',levels = levels)
+    
+    fmt = {}
+    strs = [ r'3$\sigma$', r'2$\sigma$',r'1$\sigma$']
+    for l, s in zip(CS.levels, strs):
+        fmt[l] = s
+    plt.clabel(CS, inline=True, fmt=fmt, fontsize=8)
+    
+    
     plt.plot(Ic_list[Ic_ind],Is_list[Is_ind],"xr")
     if IcPlusIs==True:
         plt.xlabel('Ic + Is [/s]')
     else:
         plt.xlabel('Ic [/s]')
     plt.ylabel('Is [/s]')
-    plt.title('Map of log likelihood')
+    plt.title('Map of log likelihood. Bin size = {:g}s'.format(effExpTime) )
     
     matplotlib.rcParams.update(oldstyle)
 
@@ -689,7 +751,7 @@ if __name__ == "__main__":
             
             
         
-    if 1:
+    if 0:
         
         effExpTime = .01
         lightCurveIntensityCounts, lightCurveIntensity, lightCurveTimes = getLightCurve(ts,ts[0]/1e6,ts[-1]/1e6,effExpTime)
@@ -736,16 +798,17 @@ if __name__ == "__main__":
         var = np.var(lightCurveIntensityCounts)
 
         try:
-            IIc,IIs = muVar_to_IcIs(mu,var,effExpTime)
+            IIc,IIs = np.asarray(muVar_to_IcIs(mu,var,effExpTime))*effExpTime
         except:
+            print('\nmuVar_to_IcIs failed\n')
             IIc = mu/2       #just create a reasonable seed 
             IIs = mu - IIc
-            IIc/=effExpTime
-            IIs/=effExpTime
+#            IIc/=effExpTime
+#            IIs/=effExpTime
         
         Ic,Is,res = maxBinMRlogL(lightCurveIntensityCounts, Ic_guess=IIc, Is_guess=IIs)
         
-        
+        print('\nIc,Is = ', Ic, Is)
         print("[Done]\n")
         
         print("=====================================")
@@ -757,16 +820,56 @@ if __name__ == "__main__":
         Make a map of log likelihood with Is and Ic+Is for the axes. 
         """
         
+        effExpTime = .001
+        lightCurveIntensityCounts, lightCurveIntensity, lightCurveTimes = getLightCurve(ts,ts[0]/1e6,ts[-1]/1e6,effExpTime)
+        
+        
+        print("Mapping...")
+        Is_list=np.linspace(20,35,25)
+        IcpIs_list=np.linspace(300,350,25)  #linspace(start,stop,number of steps)
+
+        X,Y,im = plotLogLMap(lightCurveIntensityCounts, IcpIs_list, Is_list, effExpTime,IcPlusIs = True)
+        
+
+#############################################################################################
+     
+        
         effExpTime = .01
         lightCurveIntensityCounts, lightCurveIntensity, lightCurveTimes = getLightCurve(ts,ts[0]/1e6,ts[-1]/1e6,effExpTime)
         
         
         print("Mapping...")
-        Is_list=np.linspace(25,35,25)
-        IcpIs_list=np.linspace(320,340,25)  #linspace(start,stop,number of steps)
+        Is_list=np.linspace(20,35,25)
+        IcpIs_list=np.linspace(300,350,25)  #linspace(start,stop,number of steps)
 
         X,Y,im = plotLogLMap(lightCurveIntensityCounts, IcpIs_list, Is_list, effExpTime,IcPlusIs = True)
         
+#############################################################################################
+        
+        
+#        effExpTime = .5
+#        lightCurveIntensityCounts, lightCurveIntensity, lightCurveTimes = getLightCurve(ts,ts[0]/1e6,ts[-1]/1e6,effExpTime)
+#        
+#        
+#        print("Mapping...")
+#        Is_list=np.linspace(5,20,25)
+#        IcpIs_list=np.linspace(300,350,25)  #linspace(start,stop,number of steps)
+#
+#        X,Y,im = plotLogLMap(lightCurveIntensityCounts, IcpIs_list, Is_list, effExpTime,IcPlusIs = True)
+#        
+##############################################################################################       
+#        
+#        
+#        effExpTime = 1.
+#        lightCurveIntensityCounts, lightCurveIntensity, lightCurveTimes = getLightCurve(ts,ts[0]/1e6,ts[-1]/1e6,effExpTime)
+#        
+#        
+#        print("Mapping...")
+#        Is_list=np.linspace(3,15,25)
+#        IcpIs_list=np.linspace(300,350,25)  #linspace(start,stop,number of steps)
+#
+#        X,Y,im = plotLogLMap(lightCurveIntensityCounts, IcpIs_list, Is_list, effExpTime,IcPlusIs = True)
+#        
 
         
     
