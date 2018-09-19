3f568ab9385ffddc8c11e4007e9f9e149123e3a6
captain-kay <k_davis@physics.ucsb.edu>
2018-09-19 14:23:26 -0700

    few additional bug fixes, mostly typos

--- mkidpipeline/hdf/parsebin.pyx
+++ mkidpipeline/hdf/parsebin.pyx
@@ -4,7 +4,7 @@ import numpy as np
 cimport numpy as np
 
 cdef extern from "binlib.h":
-    long parsebin(const char *file, unsigned long max_len, int* baseline, int* wavelength, unsigned long* timestamp, unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach)
+    long parsebin(const char *fName, unsigned long max_len, int* baseline, int* wavelength, unsigned long* timestamp, unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach)
     #long parsebin(const char *file, unsigned long max_len,
      #             double* a1, double *a2, double* a3);
 
@@ -16,15 +16,15 @@ def parse(file, n=0):
     y = np.empty(n, dtype=np.int)
     x = np.empty(n, dtype=np.int)
 
-    r = parsebin(file.encode('UTF-8'), n,
-                 <double*> np.PyArray_DATA(baselne),
-                 <double*> np.PyArray_DATA(phase))
-                 <double*> np.PyArray_DATA(tstamp))
-                 <double*> np.PyArray_DATA(y))
-                 <double*> np.PyArray_DATA(x))
+    r = parsebin(file.encode('UTF-8'), n)
+                 #<double*> np.PyArray_DATA(baselne),
+                 #<double*> np.PyArray_DATA(phase)
+                 #<double*> np.PyArray_DATA(tstamp)
+                 #<double*> np.PyArray_DATA(y)
+                 #<double*> np.PyArray_DATA(x))
 
     if r>n:
         return parse(file,abs(r))
 
-    ret = np.array([baselne[:r],phase[:r]], dtype=[('x', float), ('y', int)])
+    ret = np.array([baselne[:r],phase[:r],tstamp[:r],y[:r],x[:r]], dtype=['baseline', int),'wavelength', int),'timestamp', float64),('y', int), ('x', int)])
     return ret.view(np.recarray)
