34ee72d638867d32ef28dc9243a7f490d491f670
captain-kay <k_davis@physics.ucsb.edu>
2018-09-19 11:43:10 -0700

    replace Jeb's dummy code with Ben's code chopped from bin2hdf.c

--- mkidpipeline/hdf/binlib.c
+++ mkidpipeline/hdf/binlib.c
@@ -1,25 +1,117 @@
+/*
+binlib.c
+Written by Ben 9/2018
+Mostly chopped from the more involved bin2hdf
 
-long parsebin(const char* file, unsigned long a_len, double* a1, double* a2) {
+The code is basic. It reads a bin file, turns the bits in the packet into arrays based on their location in the packet.
 
-    unsigned long i=0,out_i=0;
+*/
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <netdb.h>
+//#include <sys/time.h>
+//#include <signal.h>
+//#include <time.h>
+//#include <errno.h>
+//#include <pthread.h>
+//#include <semaphore.h>
+//#include <fcntl.h>
+//#include <sys/stat.h>
+//#include <math.h>
+
+struct datapacket {
+    int baseline:17;
+    int wvl:18;
+    unsigned int timestamp:9;
+    unsigned int ycoord:10;
+    unsigned int xcoord:10;
+}__attribute__((packed));;
+
+struct hdrpacket {
+    unsigned long timestamp:36;
+    unsigned int frame:12;
+    unsigned int roach:8;
+    unsigned int start:8;
+}__attribute__((packed));;
+
+long parsebin(const char *file, unsigned long max_len,
+                       int* baseline, int* wavelength, unsigned long* timestamp,
+                       unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach) {
+    /*
+    The function returns the number of packet in the file. If the file turns out to have more packets than max_len,
+    the arrays are populated with the first max_len-1 records and the last record.
+    If there are errors (e.g. file not found) return appropriate error numbers as - return values.
+    */
+    unsigned long i=0,out_i=0, j, k, pcount=0;
     bool more_recs=true;
+	FILE *fp;
+	//struct stat st;
+	//long fSize;
+	long rd;
+	uint64_t *data;
+	uint64_t swp,swp1,firstHeader,pstart,curtime=0, curroach=0;
+	struct hdrpacket *hdr;
+	struct datapacket *photondata;
+	char packet[808*16];
 
     //open up file
+	//stat(fName, &st);
+	//fSize = st.st_size;
+	//printf("\nReading %s - %ld bytes\n",fName,fSize);
+	data = (uint64_t *) malloc(max_len*8);
+    fp = fopen(fName, "rb");
+    rd = fread( data, 1, max_len*8, fp);
+    if( rd != fSize) printf("Didn't read the entire file %s\n",fName);
+    fclose(fp);
 
-    //if not open return -1
+    //if not open
+    if (rd < 0 ) then return -1;
 
-    while(more_recs) {
-        //parse next data
+	// Find the first header packet
+	for( j=0; j<max_len; j++) {
+		swp = *((uint64_t *) (&data[j]));
+		swp1 = __bswap_64(swp);
+		hdr = (struct hdrpacket *) (&swp1);
+		if (hdr->start == 0b11111111) {
+			firstHeader = j;
+			pstart = j;
+			curtime = hdr->timestamp;
+			curroach = hdr->roach;
+			if( firstHeader != 0 ) printf("First header at %ld\n",firstHeader);
+			break;
+		}
+	}
 
-        //store data, will overwrite last entry once arrays are full
-        a1[out_i]=1337;
-        a2[out_i]=1337;
-        more_recs = i<10 ? true:false;
-        i++;
-        if (i<a_len) out_i=i;
-    }
+	// New approach - do it all in this function
+    for( k=firstHeader+1; k<max_len; k++) {
+        swp = *((uint64_t *) (&data[k]));
+        swp1 = __bswap_64(swp);
+        hdr = (struct hdrpacket *) (&swp1);
+        if (hdr->start == 0b11111111) {        // found new packet header - update timestamp and curroach
+			curtime = hdr->timestamp;
+			curroach = hdr->roach;
+		}
+		else {
+			photondata = (struct datapacket *) (&swp1);
+			baseline[pcount] = photondata->baseline;
+			wavelength[pcount] = photondata->wvl;
+			timestamp[pcount] = photondata->timestamp + curtime;
+			ycoord[pcount] = photondata->ycoord;
+			xcoord[pcount] = photondata->xcoord;
+			roach[pcount] = curroach;
+			pcount++;
+		}
+
+	}
 
     //close up file
+	free(data);
 
-    return i;
+    return pcount;
 }
--- mkidpipeline/utils/parsePacketDump.py
+++ mkidpipeline/utils/parsePacketDump.py
@@ -2,6 +2,9 @@
 File:      parsePacketDump.py
 Author:    Matt Strader
 
+
+KD Comments Sept 2018
+    Seems to be a QXcbConnection error when running main() as verbose
 """
 
 import os
@@ -14,10 +17,14 @@ import numpy as np
 from mkidpipeline.utils import binTools
 from mkidpipeline.utils.arrayPopup import plotArray
 
+# For debugging
+from time import time
+
 nRows = 125
 nCols = 80
 
 def parsePacketData(words,verbose=False):
+    STinp=time()
 
     nWords = len(words)
 
@@ -38,9 +45,11 @@ def parsePacketData(words,verbose=False):
         ax.set_title('frame size')
         print(np.max(np.diff(headerIdx)),'max frame size')
 
-    fakeIdx = np.where(words == fakePhotonWord)[0]
-    if verbose:
-        print(len(fakeIdx),'fake photons')
+    # Unused variables...so removed KD 2018
+    #fakeIdx = np.where(words == fakePhotonWord)[0]
+    #if verbose:
+        #print(len(fakeIdx),'fake photons')
+    #Tfinishedwheres = time()
 
     #header format: 8 bits all ones, 8 bits roach num, 12 bits frame num, ufix36_1 bit timestamp
     nBitsHdrTstamp = 36
@@ -48,7 +57,7 @@ def parsePacketData(words,verbose=False):
     nBitsHdrNum = 12
     nBitsHdrRoach = 8
 
-    roachNumMask = binTools.bitmask(nBitsHdrRoach)
+    roachNumMask = binTools.bitmask(nBitsHdrRoach) # Returns an int, here 255 returns:int('1'*nBits,2)
     roachNums = (headers >> (nBitsHdrNum+nBitsHdrTstamp)) & roachNumMask
     roachList = np.unique(roachNums)
     if verbose:
@@ -57,6 +66,7 @@ def parsePacketData(words,verbose=False):
     frameNumMask = binTools.bitmask(nBitsHdrNum)
     frameNums = (headers >> nBitsHdrTstamp) & frameNumMask
     frameNumDiff = np.diff(frameNums)
+    TfinishedRoachbins = time()
 
     #for roach in roachList:
     #    frameNumsByRoach.append(frameNums[roachNums==roach])
@@ -68,10 +78,10 @@ def parsePacketData(words,verbose=False):
         ax.set_title('frame nums')
         print(nMissedFrames,'missed frames')
 
-
     realIdx = np.where(np.logical_and(firstBytes != headerFirstByte, words != fakePhotonWord))[0]
     realPhotons = words[realIdx]
     nRealPhotons = len(realPhotons)
+
     if verbose:
         print(nRealPhotons,'real photons parsed')
     #photon format: 20 bits id, 9 bits ts, fix18_15 phase, fix17_14 base
@@ -97,7 +107,10 @@ def parsePacketData(words,verbose=False):
     #realIdx = realIdx[selectMask]
     #realPhotons = realPhotons[selectMask]
 
+    tstartssort = time()
     photonsHeaderIdx = np.searchsorted(headerIdx,realIdx)-1
+    tendssort = time()
+    print("searchsortes took %f sec" % (tendssort - tstartssort))
 
     photonsHeader = headers[photonsHeaderIdx]
     #now get the timestamp from this
@@ -133,27 +146,36 @@ def parsePacketData(words,verbose=False):
     return {'basesDeg':basesDeg,'phasesDeg':phasesDeg,'photonTimestamps':photonTimestamps,'pixelIds':pixelIds,'photons':realPhotons,'headers':headers,'roachNums':roachNums,'image':image,'xCoords':xCoords,'yCoords':yCoords}
 
 if __name__=='__main__':
+    startT = time()
+
     if len(sys.argv) > 1:
         path = sys.argv[1]
     else:
         path = 'photonDump.bin'
 
     pathTstamp = os.path.splitext(os.path.basename(path))[0]
-    with open(path,'rb') as dumpFile:
-        data = dumpFile.read()
+    try:
+        with open(path,'rb') as dumpFile:
+            data = dumpFile.read()
+    except (IOError, ValueError) as e:
+        print(e)
+        exit(1)
 
     nBytes = len(data)
-    nWords = nBytes/8 #64 bit words
+    nWords = nBytes//8 #64 bit words
     #break into 64 bit words
     words = np.array(struct.unpack('>{:d}Q'.format(nWords), data),dtype=object)
 
-    parseDict = parsePacketData(words,verbose=True)
+    STparse = time()
+    parseDict = parsePacketData(words,verbose=False)
+    ETparse = time()
+    print("parsing took %f sec" % (ETparse - STparse))
 
     phasesDeg = parseDict['phasesDeg']
     basesDeg = parseDict['basesDeg']
     pixelIds = parseDict['pixelIds']
     image = parseDict['image']
-
+'''
     #selPixelId = 0#(1<<10)+23
     selPixelId = (30<<10)+46
     print('selected pixel',selPixelId)
@@ -174,3 +196,4 @@ if __name__=='__main__':
 
 
     plt.show()
+'''
