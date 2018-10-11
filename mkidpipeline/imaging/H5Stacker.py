e201e9a8c2c70f947ec0fb0cd1e2fdfae68a5cb0
Isabel Lipartito <ilipartito@umail.ucsb.edu>
2018-10-11 13:42:35 -0700

    Updated to work with MEC data.

--- mkidpipeline/imaging/H5Stacker.py
+++ mkidpipeline/imaging/H5Stacker.py
@@ -1,62 +1,64 @@
 #!/bin/python
 """
 Author:  Isabel Lipartito    Date 4/18/2018
-Use if you've fot a bunch of H5s that are wavelength calibrated (and part of a single Dither stack)
+Use if you've got a bunch of H5s that are wavelength calibrated (and part of a single Dither stack)
 Program reads in those H5s and use getPixelCountImage to make an image for each
 Uses the same stacking and rotation code in quickStack to align them (irUtils)
 Makes and Plots final Image
 
-Will work for non-wavelength calibrated images, but you must set the wvlStart and wvlStop to 0
+Will work for non-wavelength calibrated images, but you must set the wvlStart and wvlStop to None
 """
 
 import glob
-import os
+import ast
+import argparse
 import sys
 
-import irUtils
+
 import numpy as np
 from mkidpipeline.hdf.darkObsFile import ObsFile as obs
 from mkidpipeline.utils.arrayPopup import plotArray
-from mkidreadout.utils.readDict import readDict
+from configparser import ConfigParser
+from mkidpipeline.utils import irUtils
 
-if len(sys.argv)<2:
-    #grab most recent .cfg file
-    print("No .cfg file provided, trying to grab most recent one from Params...")
-    try:
-        configFileName = max(glob.iglob('./Params/*.cfg'), key=os.path.getctime)
-    except:
-        print("Failed to load appropriate .cfg file. Please provide path as argument")
-        sys.exit(0)
-else:
-    configFileName = sys.argv[1]
+parser = argparse.ArgumentParser(description='Process a dither stack into a final rough-stacked image.')
+parser.add_argument('ditherName', metavar='file', nargs=1,
+                    help='filename of the dither config file')
 
-print("Using config", configFileName)
+args = parser.parse_args()
+
+config_directory = sys.argv[1]
+config = ConfigParser()
+config.read(config_directory)
+XPIX = ast.literal_eval(config['Data']['XPIX'])
+YPIX = ast.literal_eval(config['Data']['YPIX'])
+binPath = ast.literal_eval(config['Data']['binPath'])  # path to raw .bin data
+outPath = ast.literal_eval(config['Data']['outPath'])   # path to output data
+beamFile = ast.literal_eval(config['Data']['beamFile'])  # path and filename to beam map file
+mapFlag = ast.literal_eval(config['Data']['mapFlag'])
+filePrefix = ast.literal_eval(config['Data']['filePrefix'])
+b2hPath = ast.literal_eval(config['Data']['b2hPath'])
+nPos = ast.literal_eval(config['Data']['nPos'])
+intTime =  ast.literal_eval(config['Data']['intTime'])
+startTimes = ast.literal_eval(config['Data']['startTimes'])
+stopTimes =  ast.literal_eval(config['Data']['stopTimes'])
+xPos = ast.literal_eval(config['Data']['xPos'])
+yPos = ast.literal_eval(config['Data']['yPos'])
+useImg = ast.literal_eval(config['Data']['useImg'])
 
 
-configData = readDict()
-configData.read_from_file(configFileName)
+upSample = 2
+padFraction = 0.4
+wvlStart=0
+wvlStop=0
 
-# Extract parameters from config file
-nPos = int(configData['nPos'])
-startTimes = np.array(configData['startTimes'], dtype=int)
-stopTimes = np.array(configData['stopTimes'], dtype=int)
-xPos = np.array(configData['xPos'], dtype=int)
-yPos = np.array(configData['yPos'], dtype=int)
-numRows = int(configData['numRows'])
-numCols = int(configData['numCols'])
-upSample = int(configData['upSample'])
-padFraction = float(configData['padFraction'])
-coldCut = int(configData['coldCut'])
-target = str(configData['target'])
-run = str(configData['run'])
-date = str(configData['date'])
-wvlStart=int(configData['wvlStart'])
-wvlStop=int(configData['wvlStop'])
-h5dir = str(configData['h5dir'])
-outputDir = str(configData['outputDir'])
-outfileName=str(configData['outfileName'])
+h5dir = outPath
+print(h5dir)
+outputDir = outPath
+outfileName='HR8799StackWaveCal'
 
-ObsFNList =glob.glob(h5dir+'15*.h5')  
+ObsFNList =glob.glob(h5dir+'15*.h5')
+print(ObsFNList)
 
 rawImgs=[]
 roughShiftsX=[]
@@ -73,15 +75,15 @@ refPointY = yPos[0]
 print('xPos',xPos[0],'yPos',yPos[0])
 
 #determine the coarse x and y shifts that subsequent frames must be moved to align with first frame
-dXs = refPointX-xPos
-dYs = refPointY-yPos
+dXs = (np.zeros(len(xPos))+refPointX)-xPos
+dYs = (np.zeros(len(yPos))+refPointY)-yPos
 
 
 #initialize hpDict so we can just check if it exists and only make it once
 hpDict=None
 
 #to ensure stacking is weighting all dither positions evenly, we need to know the one with the shortest integration
-intTime = min(stopTimes-startTimes)
+intTime = min(np.array(stopTimes)-np.array(startTimes))
 firstSec=0
 print("Shortest Integration time = ", intTime)
 
@@ -92,14 +94,19 @@ for i in range(nPos):
         #be used from each position
         obsfile=obs(ObsFNList[i], mode='write')
         if wvlStart != 0 and wvlStop!=0:
-           img = obsfile.getPixelCountImage(firstSec =0, integrationTime=intTime,applyWeight=False,flagToUse = 0,wvlRange = (wvlStart,wvlStop))
-           print('Running getPixelCountImage on ',firstSec,'seconds to ',intTime,'seconds of data from wavelength ',wvlStart,'to ',wvlStop)
+            img = obsfile.getPixelCountImage(firstSec =0, integrationTime=intTime,applyWeight=False,flagToUse = 0,wvlStart=wvlStart,wvlStop=wvlStop)
+            print('Running getPixelCountImage on ',firstSec,'seconds to ',intTime,'seconds of data from wavelength ',wvlStart,'to ',wvlStop)
         else:
-           img = obsfile.getPixelCountImage(firstSec =0, integrationTime=intTime,applyWeight=False,flagToUse = 0,wvlRange = None)
-           print('Running getPixelCountImage on ',firstSec,'seconds to ',intTime,'seconds of data on all wavelengths')
+            img = obsfile.getPixelCountImage(firstSec =0, integrationTime=intTime,applyWeight=False,flagToUse = 0,wvlStart=None,wvlStop=None)
+            print('Running getPixelCountImage on ',firstSec,'seconds to ',intTime,'seconds of data on all wavelengths')
+
+        saveim = np.transpose(img['image'])
+        outfile = h5dir + outfileName + 'DitherPosition%i'%i
+        np.save(outfile, saveim)
+
         processedIm = np.transpose(img['image'])/intTime
         print(np.shape(processedIm))
-        processedIm=processedIm[50:124, 0:80]
+        #processedIm=processedIm[50:124, 0:80]
 
         roughShiftsX.append(dXs[i])
         roughShiftsY.append(dYs[i])
@@ -110,7 +117,7 @@ for i in range(nPos):
         if i==0:
             plotArray(processedIm,title='Dither Pos %i'%i,origin='upper',vmin=0)
         #cut out cold/dead pixels
-        processedIm[np.where(processedIm<=coldCut)]=np.nan
+        processedIm[np.where(processedIm>=2400)]=np.nan
 
         #plot an example of the masked image for inspection
         if i==0:
@@ -142,5 +149,5 @@ shiftedFrames = np.array(ditherFrames)
 finalImage = irUtils.medianStack(shiftedFrames)
 
 plotArray(finalImage,title='final',origin='upper')
-outfile=h5dir+outfileName+'Aligned'
+outfile=h5dir+outfileName+'AlignedWaveCal'
 np.save(outfile, finalImage)
