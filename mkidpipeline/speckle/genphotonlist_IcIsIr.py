e899fb8823de935c07dab6870ad3b1ef663e80f2
Isabel Lipartito <ilipartito@umail.ucsb.edu>
2018-11-29 15:25:12 -0800

    Added function 'genphotonlist2D', generates photon lists for a 2D array of (Ic, Is) that are spatially as well as temporally correlated. Writes photonlists to h5 files in the same format as obsfiles

--- mkidpipeline/speckle/genphotonlist_IcIsIr.py
+++ mkidpipeline/speckle/genphotonlist_IcIsIr.py
@@ -5,6 +5,10 @@ import numpy as np
 from scipy import special, interpolate
 import pyximport; pyximport.install()
 import mkidpipeline.speckle.photonstats_utils as utils
+from astropy.convolution import Gaussian2DKernel, convolve
+from mkidcore.headers import ObsHeader
+from mkidcore.headers import ObsFileCols
+import tables
 
 def MRicdf(Ic, Is, interpmethod='cubic'):
 
@@ -123,7 +127,7 @@ def genphotonlist(Ic, Is, Ir, Ttot, tau, deadtime=0, interpmethod='cubic',
 
     """
 
-    # Generate a correlated Gaussian sequence, corrlation time tau.
+    # Generate a correlated Gaussian sequence, correlation time tau.
     # Then transform this to a random variable uniformly distributed
     # between 0 and 1, and finally back to a modified Rician random
     # variable.  This method ensures that: (1) the output is M-R
@@ -189,12 +193,145 @@ def genphotonlist(Ic, Is, Ir, Ttot, tau, deadtime=0, interpmethod='cubic',
         return [tlist_tot[indx][ikeep], plist_tot[indx][ikeep]]
     
     return tlist_tot[indx][np.where(keep)]
-    
+
+def diffrac_lim_kernel(input_image, diffrac_lim=2.68, *argsconvolve,**kwargsconvolve):
+    [x_size, y_size] = np.shape(input_image)
+    sigma = diffrac_lim / (2. * math.sqrt(2. * math.log(2.)))
+    gaussian_2D_kernel = Gaussian2DKernel(sigma, x_size, y_size)
+    convolved_kernel = convolve(input_image, gaussian_2D_kernel, *argsconvolve,**kwargsconvolve)
+    return convolved_kernel
+
+def genphotonlist2D(Ic, Is, Ir, Ttot, tau, out_directory, deadtime=0, interpmethod='cubic',
+                  taufac=500, diffrac_lim=2.86):
+    """
+    Same functionality as genphotonlist except this function is intended to iterate over an entire 2D (Ic,Is) map
+    It makes sure the intensities generated from corrsequence are correlated spatially (they will be correlated temporally)
+
+    """
+
+    # Generate a correlated Gaussian sequence, correlation time tau.
+    # Then transform this to a random variable uniformly distributed
+    # between 0 and 1, and finally back to a modified Rician random
+    # variable.  This method ensures that: (1) the output is M-R
+    # distributed, and (2) it is exponentially correlated.  Finally,
+    # return a list of photons determined by the probability of each
+    # unit of time giving a detected photon.
+
+    # Number of microseconds per bin in which we discretize intensity
+
+    N = max(int(tau * 1e6 / taufac), 1)
+
+    t_base, N_base=corrsequence(int(Ttot * 1e6 / N), tau * 1e6 / N)
+    x_size, y_size=Ic.shape
+    intensity_cube=np.zeros(x_size, y_size, t_base)
+
+    for x_pix in range(x_size):
+        for y_pix in range(y_size):
+
+            if Is[x_pix,y_pix] > 1e-8 * Ic[x_pix,y_pix]:
+                t, normal = corrsequence(int(Ttot * 1e6 / N), tau * 1e6 / N)
+                intensity_cube[x_pix,y_pix, :]=normal
+
+            elif Is[x_pix,y_pix] >= 0 and Ic[x_pix, y_pix] >=0:
+                N = max(N, 1000)
+                t = np.arange(int(Ttot * 1e6))
+                intensity_cube[x_pix,y_pix, :]=Ic / 1e6 * np.ones(t.shape)
+
+            else:
+                intensity_cube[x_pix, y_pix, :] =  np.full_like(intensity_cube[x_pix, y_pix, :], np.nan)
+
+    for timestamp in range(t_base):
+        intensity_cube[:,:,timestamp]=diffrac_lim_kernel(intensity_cube[:,:,timestamp], diffrac_lim=diffrac_lim,
+                                                         nan_treatment='interpolate', preserve_nan=True)
+
+    photon_table = tables.open_file(out_directory, mode='w')
+    Header = photon_table.create_group(photon_table.root, 'header', 'header')
+    header=photon_table.create_table(Header, 'Header', ObsHeader, title='header')
+    Photons = photon_table.create_group(photon_table.root, 'Photons', 'Photons')
+    PhotonTable = photon_table.create_table(Photons, 'PhotonTable', ObsFileCols, title='Photon Data')
+
+    head=header.row
+
+    head['beammapFile']=''
+    head['dataDir']=''
+    head['energyBinWidth']=0.1
+    head['expTime'] = 0.0
+    head['isFlatCalibrated'] = False
+    head['isLinearityCorrected']= False
+    head['isPhaseNoiseCorrected']=False
+    head['isPhotonTailCorrected']=False
+    head['isSpecCalibrated']=False
+    head['isWvlCalibrated']=False
+    head['isFlatCalibrated']=False
+    head['startTime']=0
+    head['target']='CHARIS'
+    head['timeMaskExists']=False
+    head['wvlBinStart']=700.0
+    head['wvlBinEnd']=700.0
+    head['wvlCalFile']=''
+    head.append()
+
+    photon = PhotonTable.row
+
+    iteration=0
+    for x_pix in range(x_size):
+        for y_pix in range(y_size):
+
+            if Is[x_pix, y_pix] > 1e-8 * Ic[x_pix, y_pix]:
+                normal=intensity_cube[x_pix,y_pix,:]
+                uniform = 0.5 * (special.erf(normal / np.sqrt(2)) + 1)
+                t *= N
+                f = MRicdf(Ic, Is, interpmethod=interpmethod)
+                I = f(uniform) / 1e6
+                intensity_cube[x_pix, y_pix, :] = I
+
+            # Number of photons from each distribution in each time bin
+            I=intensity_cube[x_pix,y_pix,:]
+            n1 = np.random.poisson(I * N)
+            n2 = np.random.poisson(np.ones(t.shape) * Ir / 1e6 * N)
+
+            # Go ahead and make the list with repeated times
+
+            tlist = t[np.where(n1 > 0)]
+            tlist_r = t[np.where(n2 > 0)]
+
+            for i in range(1, max(np.amax(n1), np.amax(n2)) + 1):
+                tlist = np.concatenate((tlist, t[np.where(n1 > i)]))
+                tlist_r = np.concatenate((tlist_r, t[np.where(n2 > i)]))
+
+            tlist_tot = np.concatenate((tlist, tlist_r)) * 1.
+
+            # Add a random number to give the exact arrival time within the bin
+
+            tlist_tot += N * np.random.rand(len(tlist_tot))
+
+            # Cython is much, much faster given that this has to be an
+            # explicit for loop; without Cython (even with numba) this step
+            # would dominate the run time.  Returns indices of the times we
+            # keep.
+
+            indx = np.argsort(tlist_tot)
+            keep = utils.removedeadtime(tlist_tot[indx], deadtime)
+
+            final_tlist=tlist_tot[indx][np.where(keep)]
+            for Time in final_tlist:
+                photon['ResID'] = float(iteration)
+                photon['Wavelength'] = 700.0
+                photon['SpecWeight'] = 1.0
+                photon['NoiseWeight'] = 1.0
+                photon['Time'] = Time
+                photon.append()
+
+            iteration+=1
+
+    photon_table.flush()
+    photon_table.close()
+
 if __name__ == "__main__":
 
     # Demonstration: Ic=1000/s, Is=300/s, Ir=500/s, 5s integration,
     # decorrelation time 0.1s.  Returns list of ~9000 times.
 
     Ic, Is, Ir, Ttot, tau = [1000, 300, 500, 5, 0.1]
-    t, p = genphotonlist(Ic, Is, Ir, Ttot, tau, deadtime=10, return_IDs=True)
+    t, p = genphotonlist2D(Ic, Is, Ir, Ttot, tau, deadtime=10, return_IDs=True)
     
