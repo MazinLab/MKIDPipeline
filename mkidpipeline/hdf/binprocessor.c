1b8a307db69ee5185e806d491c7fb306a9ea7545
Jeb Bailey <baileyji@umich.edu>
2019-01-18 14:32:30 -0800

    Merge parsebin & binprocessor

--- mkidpipeline/hdf/binprocessor.c
+++ mkidpipeline/hdf/binprocessor.c
@@ -461,3 +461,80 @@ long extract_photons_dummy(const char *binpath, unsigned long start_timestamp, u
     return n_max_photons;
 
 }
+
+
+long cparsebin(const char *fName, unsigned long max_len,
+                       int* baseline, float* wavelength, unsigned long* timestamp,
+                       unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach) {
+    /*
+    The function returns the number of packet in the file. If the file turns out to have more packets than max_len,
+    the arrays are populated with the first max_len-1 records and the last record.
+    If there are errors (e.g. file not found) return appropriate error numbers as - return values.
+    */
+    unsigned long out_i=0, pcount=0;
+	FILE *fp;
+	struct stat st;
+	long fSize,rd;
+	uint64_t *data;
+	uint64_t swp,swp1,firstHeader,pstart,curtime=0,curroach=0;
+	struct hdrpacket *hdr;
+	struct datapacket *photondata;
+	char packet[808*16];
+
+    //open up file
+	stat(fName, &st);
+	fSize = st.st_size;
+	//printf("\nReading %s - %ld bytes\n",fName,fSize);
+	data = (uint64_t *) malloc(fSize);
+    fp = fopen(fName, "rb");
+    rd = fread( data, 1, fSize, fp);
+    if( rd != fSize) printf("Didn't read the entire file %s\n",fName);
+    fclose(fp);
+
+    //if not open
+    if (rd < 0 ) return -1;
+
+	// Find the first header packet
+	for(unsigned long i=0; i<fSize/8; i++) {
+		swp = *((uint64_t *) (&data[i]));
+		swp1 = bswap_64(swp);
+		hdr = (struct hdrpacket *) (&swp1);
+		if (hdr->start == 0b11111111) {
+			firstHeader = i;
+			pstart = i;
+			curtime = hdr->timestamp;
+			curroach = hdr->roach;
+			if( firstHeader != 0 ) printf("First header at %ld\n",firstHeader);
+			break;
+		}
+	}
+
+	// New approach - do it all in this function
+    for(unsigned long i=firstHeader+1; i<fSize/8; i++) {
+        swp = *((uint64_t *) (&data[i]));
+        swp1 = bswap_64(swp);
+        hdr = (struct hdrpacket *) (&swp1);
+        if (hdr->start == 0b11111111) {        // found new packet header - update timestamp and curroach
+			curtime = hdr->timestamp;
+			curroach = hdr->roach;
+		}
+		else {                              // must be data. Save as photondata struct
+		    out_i = pcount >= max_len ? max_len: pcount;
+			photondata = (struct datapacket *) (&swp1);
+			baseline[out_i] = photondata->baseline;
+			wavelength[out_i] = ((float) photondata->wvl)*RAD2DEG/32768.0;
+			timestamp[out_i] = photondata->timestamp + curtime;
+			ycoord[out_i] = photondata->ycoord;
+			xcoord[out_i] = photondata->xcoord;
+			roach[out_i] = curroach;
+			//wavelength[out_i] = wavelength[out_i]*RAD2DEG/32768.0;
+			pcount++;
+		}
+
+	}
+    //wavelength = ((float) data->wvl)*RAD2DEG/32768.0;
+    //close up file
+	free(data);
+
+    return pcount;
+}
--- mkidpipeline/hdf/mkidbin.pyx
+++ mkidpipeline/hdf/mkidbin.pyx
@@ -1,3 +1,10 @@
+"""
+When updating this code:
+You must also recompile the setup.py file located in the current directory
+which containes the information to make(compile) the cythonized file.
+(in current repository, in .../MKIDPipeline dir)
+$ python setup.py build_ext --inplace
+"""
 import numpy as np
 cimport numpy as np
 import os
@@ -13,8 +20,8 @@ cdef extern from "binprocessor.h":
                          unsigned int x, unsigned int y,unsigned long n_max_photons, photon* photons)
     long extract_photons_dummy(const char *dname, unsigned long start, unsigned long inttime, const char *bmap,
                                unsigned int x, unsigned int y, unsigned long n_max_photons, photon* photons)
-#    long cparsebin(const char *fName, unsigned long max_len, int* baseline, float* wavelength,
-#                   unsigned long* timestamp, unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach)
+    long cparsebin(const char *fName, unsigned long max_len, int* baseline, float* wavelength,
+                   unsigned long* timestamp, unsigned int* ycoord, unsigned int* xcoord, unsigned int* roach)
 
 
 np_photon = np.dtype([('resID',np.uint32),
@@ -70,49 +77,79 @@ def test(nphot=10):
     return photons
 
 
-# def parse(file,_n=0):
-#
-#     # Creating pointers to memory bocks that the binlib.c code will fill
-#     n = int(max(os.stat(file).st_size/8, _n))
-#     baseline   = np.empty(n, dtype=np.int)
-#     wavelength = np.empty(n, dtype=np.float32)
-#     timestamp  = np.empty(n, dtype=np.uint64)
-#     y = np.empty(n, dtype=np.uint32)
-#     x = np.empty(n, dtype=np.uint32)
-#     roachnum = np.empty(n, dtype=np.uint32)
-#
-#     # Calling parsebin from binlib.c
-#     # npackets is the number of real+fake photons processed by binlib.c
-#     npackets = cparsebin(file.encode('UTF-8'), n,
-#                  <int*>np.PyArray_DATA(baseline),
-#                  <float*>np.PyArray_DATA(wavelength),
-#                  <unsigned long*>np.PyArray_DATA(timestamp),
-#                  <unsigned int*>np.PyArray_DATA(y),
-#                  <unsigned int*>np.PyArray_DATA(x),
-#                  <unsigned int*>np.PyArray_DATA(roachnum))
-#     #print("number of parsed photons = {}".format(npackets))
-#
-#     if npackets>n:
-#         return parse(file,abs(npackets))
-#     elif npackets<0:
-#         errors = {-1:'Data not found'}
-#         raise RuntimeError(errors.get(npackets, 'Unknown Error: {}'.format(npackets)))
-#
-#     # Create new struct from output from binlib.c
-#     # What this does is only grab the elements of the arrays that are real valued
-#     # This essentially clips the data since we declared it to be as long as the .bin
-#     #  file, but some of those lines were headers, which are now empty in the returned arrays
-#     #  We also combine the arrays into a single struct. It's a for loop, but its actually a fast one
-#     dt  = np.dtype([('baseline', int),('phase', float), ('tstamp', np.float64),('y', int), ('x', int),('roach', int)])
-#     cdef p = np.zeros(npackets,dtype=dt)
-#     for name, x in zip(dt.names, [baseline[:npackets],wavelength[:npackets],
-#                                   timestamp[:npackets],y[:npackets],x[:npackets],roachnum[:npackets]]):
-#         p[name] = x
-#     p = p.view(np.recarray)
-#
-#     # Remove Fake Photons
-#     #   fake photons translate to x=511 when you read the bitvalues as numbers
-#     #   we just throw them away since they aren't useful to anybody
-#     ret = np.delete(p, p.x==511)
-#
-#     return ret
+def parse(file,_n=0):
+    """
+    Created by KD and JB Sept 2018
+
+    This cython function will take an input .bin file and send it to be parsed to
+    with a .c file and return the data as a python struct. The parsing is
+    done in a program binprocessor.c/binprocessor.h
+    The parsed data has the full timestamp created from adding the header and
+    data packet timestamps in the .bin files.
+
+    The input .bin file to the function contains both header packets and
+    "fake photon" data, which is a photon created by the firmware to end an
+    ethernet frame. The firmware is set to end a fframe either when there are
+    100 photons in the frame or the clock reaches the next 0.5 ms. In the latter
+    casse, the firmware MUST generate a fake photon to end the frame. These
+    fake photons are not removed in the binlib.c code (they could be. We remove
+    them here in the cython file by creating a new numpy array through the
+    np.delete function. We have to create a new array, even though it is memory
+    intensive, because you cannot change a numpy array's shape (easily).
+
+    File is called by:
+    p = mkidbin.parse("fname.bin",n)
+
+    Returns
+    p.base = baseline
+    p.phase = phase
+    p.tstamp = timestamp
+    p.y = ycoord
+    p.x = xcoord
+    p.roach = roachnum
+    """
+
+    # Creating pointers to memory bocks that the binlib.c code will fill
+    n = int(max(os.stat(file).st_size/8, _n))
+    baseline   = np.empty(n, dtype=np.int)
+    wavelength = np.empty(n, dtype=np.float32)
+    timestamp  = np.empty(n, dtype=np.uint64)
+    y = np.empty(n, dtype=np.uint32)
+    x = np.empty(n, dtype=np.uint32)
+    roachnum = np.empty(n, dtype=np.uint32)
+
+    # Calling parsebin from binlib.c
+    # npackets is the number of real+fake photons processed by binlib.c
+    npackets = cparsebin(file.encode('UTF-8'), n,
+                 <int*>np.PyArray_DATA(baseline),
+                 <float*>np.PyArray_DATA(wavelength),
+                 <unsigned long*>np.PyArray_DATA(timestamp),
+                 <unsigned int*>np.PyArray_DATA(y),
+                 <unsigned int*>np.PyArray_DATA(x),
+                 <unsigned int*>np.PyArray_DATA(roachnum))
+    #print("number of parsed photons = {}".format(npackets))
+
+    if npackets>n:
+        return parse(file,abs(npackets))
+    elif npackets<0:
+        errors = {-1:'Data not found'}
+        raise RuntimeError(errors.get(npackets, 'Unknown Error: {}'.format(npackets)))
+
+    # Create new struct from output from binlib.c
+    # What this does is only grab the elements of the arrays that are real valued
+    # This essentially clips the data since we declared it to be as long as the .bin
+    #  file, but some of those lines were headers, which are now empty in the returned arrays
+    #  We also combine the arrays into a single struct. It's a for loop, but its actually a fast one
+    dt  = np.dtype([('baseline', int),('phase', float), ('tstamp', np.float64),('y', int), ('x', int),('roach', int)])
+    cdef p = np.zeros(npackets,dtype=dt)
+    for name, x in zip(dt.names, [baseline[:npackets],wavelength[:npackets],
+                                  timestamp[:npackets],y[:npackets],x[:npackets],roachnum[:npackets]]):
+        p[name] = x
+    p = p.view(np.recarray)
+
+    # Remove Fake Photons
+    #   fake photons translate to x=511 when you read the bitvalues as numbers
+    #   we just throw them away since they aren't useful to anybody
+    ret = np.delete(p, p.x==511)
+
+    return ret
