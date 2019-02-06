dda6958e659d92e99ade4029ba6e8f7d22a09cd6
Clint Bockstiegel <clint@physics.ucsb.edu>
2019-02-05 22:26:32 -0800

    fixed a typo

--- mkidpipeline/hdf/binprocessor.c
+++ mkidpipeline/hdf/binprocessor.c
@@ -37,7 +37,7 @@
 (((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | \
 (uint32_t)bswap_16((uint16_t)((value) >> 16)))
 
-#define bswap_64(value) \
+#define __bswap_64(value) \
 (((uint64_t)bswap_32((uint32_t)((value) & 0xffffffff)) \
 << 32) | \
 (uint64_t)bswap_32((uint32_t)((value) >> 32)))
