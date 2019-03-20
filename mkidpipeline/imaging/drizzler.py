348232c1feb368724932e20c82e93c9abd3bc9b0
rdodkins <rupertdodkins@gmail.com>
2019-03-19 22:06:36 -0700

    More integration with pipeline config. drizzler appears stable. derotate is now a bool option. Updated the TODOs

--- mkidpipeline/imaging/drizzler.py
+++ mkidpipeline/imaging/drizzler.py
@@ -1,21 +1,21 @@
 """
 TODO
-Add astroplan, ephem, drizzle, SharedArray to setup.py/yml. ephem can be conda installed, drizzle and SharedArray need to be pip
-installed. I found that  astroplan needed to be pip installed otherwise some astropy import fails
-
-Better handling of savestate of photonlists
+Add astroplan, drizzle, to setup.py/yml. drizzle need to be pip installed. I found that astroplan needed to be pip
+installed otherwise some astropy import fails
 
 Move plotting functionality to another module
+
+Update photon TOA positioning after derotation functionality
+
+Sort out centering of frame vs center of rotation (star)
 
 Usage
 -----
 
-python drizzle.py
+python drizzle.py path/to/drizzler.yml
 
-Author: Rupert Dodkins, Julian van Eyken            Date: Jan 2019
+Author: Rupert Dodkins,                                 Date: Mar 2019
 
-
-This code is adapted from Julian's testImageStack from the ARCONS pipeline.
 """
 import matplotlib
 import os
@@ -26,7 +26,6 @@ import matplotlib.pylab as plt
 from matplotlib.colors import LogNorm
 import matplotlib.ticker as ticker
 from scipy.ndimage import gaussian_filter
-import ephem
 from astropy import wcs
 from astropy.coordinates import EarthLocation, Angle, SkyCoord
 import astropy.units as u
@@ -36,12 +35,9 @@ from astropy.io import fits
 from drizzle import drizzle as stdrizzle
 from mkidcore import pixelflags
 from mkidpipeline.hdf.photontable import ObsFile
-from mkidcore.corelog import getLogger
-from mkidpipeline.config import MKIDObservingDataDescription, MKIDObservingDither
+import mkidcore.corelog as pipelinelog
 import pickle
 import mkidpipeline
-import numpy.ma as ma
-import pkg_resources as pkg
 from mkidcore.instruments import CONEX2PIXEL
 import argparse
 
@@ -62,13 +58,13 @@ class DitherDescription(object):
     TODO implement center of rotation
     """
 
-    def __init__(self, dither, virPixCen=(25, 0), observatory='Subaru', target='* kap And', rot_rate_multiplier=1):
+    def __init__(self, dither, virPixCen=(25, 0), observatory='Subaru', target='* kap And', derotate=True):
         self.description = dither
         self.target = target
         try:
             self.coords = dither.obs[0].lookup_coodinates(queryname=target)
         except astropy.coordinates.name_resolve.NameResolveError:
-            getLogger(__name__).warning('Unable to resolve coordinates for target name {target}, using (0,0).')
+            pipelinelog.getLogger(__name__).warning('Unable to resolve coordinates for target name {target}, using (0,0).')
             self.coords = self.SkyCoord('0 deg', '0 deg')
         self.cenRA, self.cenDec = self.coords.ra.deg, self.coords.dec.deg
         self.virPixCen = np.array([list(virPixCen)]).T
@@ -87,11 +83,11 @@ class DitherDescription(object):
         altaz = apo.altaz(astropy.time.Time(val=times, format='unix'), self.coords)
         earthrate = 2 * np.pi / u.sday.to(u.second)
         rot_rate = earthrate * np.cos(site.geodetic.lat.rad) * np.cos(altaz.az.radian) / np.cos(altaz.alt.radian)
-        rot_rate *= rot_rate_multiplier
+        rot_rate *= derotate
 
         self.dithHAs = [np.trapz(rot_rate[:ix], x=times[:ix] - times[0]) for ix in range(1, len(times) + 1)]
 
-        getLogger(__name__).debug("HAs: %s", self.dithHAs)
+        pipelinelog.getLogger(__name__).debug("HAs: %s", self.dithHAs)
 
     def plot(self):
         rotationMatrix = np.array([[np.cos(self.dithHAs), -np.sin(self.dithHAs)],
@@ -119,6 +115,8 @@ class Drizzler(object):
         # self.randoffset = False # apply random spatial offset to each photon
         self.nPixRA = None
         self.nPixDec = None
+        # self.nPixRA = 300
+        # self.nPixDec = 300
 
         self.config = None
         self.files = photonlists
@@ -143,15 +141,13 @@ class Drizzler(object):
 
         # self.cenRA = (raMin + raMax) / 2.0
         # self.cenDec = (decMin + decMax) / 2.0
-        #
-        # # Set size of virtual grid to accommodate.
-        #
-        # if self.nPixRA is None:
-        #     # +1 for round up; +1 because coordinates are the boundaries of the virtual pixels, not the centers.
-        #     self.nPixRA = int((raMax - raMin) // self.vPlateScale + 2)
-        # if self.nPixDec is None:
-        #     self.nPixDec = int((decMax - decMin) // self.vPlateScale + 2)
-        self.nPixRA, self.nPixDec = 300, 300  # 250, 250
+
+        # Set size of virtual grid to accommodate.
+        if self.nPixRA is None:
+            # +1 for round up; +1 because coordinates are the boundaries of the virtual pixels, not the centers.
+            self.nPixRA = int((raMax - raMin) // self.vPlateScale + 2)
+        if self.nPixDec is None:
+            self.nPixDec = int((decMax - decMin) // self.vPlateScale + 2)
 
         self.generate_coordinate_grid()
 
@@ -168,21 +164,22 @@ class Drizzler(object):
         self.gridRA = self.cenRA + (self.vPlateScale * (np.arange(self.nPixRA + 1) - ((self.nPixRA + 1) // 2)))
         self.gridDec = self.cenDec + (self.vPlateScale * (np.arange(self.nPixDec + 1) - ((self.nPixDec + 1) // 2)))
 
-    def get_header(self):
+    def get_header(self, center_on_star=True):
         # TODO implement something like this
         # w = mkidcore.buildwcs(self.nPixRA, self.nPixDec, self.vPlateScale, self.cenRA, self.cenDec)
         # TODO implement the PV distortion?
         # eg w.wcs.set_pv([(2, 1, 45.0)])
 
         self.w = wcs.WCS(naxis=2)
-        self.w.wcs.crpix = np.array([self.nPixRA / 2., self.nPixDec / 2.]) + np.array(
-            [self.virPixCen[0][0], self.virPixCen[1][0]])
+        self.w.wcs.crpix = np.array([self.nPixRA / 2., self.nPixDec / 2.])
+        if center_on_star:
+            self.w.wcs.crpix -= np.array([self.virPixCen[0][0], self.virPixCen[1][0]])
         self.w.wcs.cdelt = np.array([self.vPlateScale, self.vPlateScale])
         self.w.wcs.crval = [self.cenRA, self.cenDec]
         self.w.wcs.ctype = ["RA-----", "DEC----"]
         self.w._naxis1 = self.nPixRA
         self.w._naxis2 = self.nPixDec
-        print(self.w)
+        pipelinelog.getLogger(__name__).debug(self.w)
 
 
 class SpectralDrizzler(Drizzler):
@@ -197,10 +194,10 @@ class SpectralDrizzler(Drizzler):
     def run(self, save_file=None):
         for ix, file in enumerate(self.files):
 
-            getLogger(__name__).debug('Processing %s', file)
+            pipelinelog.getLogger(__name__).debug('Processing %s', file)
             tic = time.clock()
             insci, inwcs = self.makeCube(file)
-            getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
+            pipelinelog.getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
             for iw in range(self.nwvlbins):
                 self.drizcube[iw].add_image(insci[iw], inwcs, inwht=np.int_(np.logical_not(insci[iw] == 0)))
 
@@ -267,7 +264,7 @@ class TemporalDrizzler(Drizzler):
         self.totWeightCube = np.zeros((self.ntimebins, self.nwvlbins, self.nPixDec, self.nPixRA))
         for ix, file in enumerate(self.files):
 
-            getLogger(__name__).debug('Processing %s', file)
+            pipelinelog.getLogger(__name__).debug('Processing %s', file)
 
             insci, inwcs = self.makeHyper(file)
 
@@ -281,7 +278,7 @@ class TemporalDrizzler(Drizzler):
 
             self.totHypCube[ix * self.ntimebins: (ix + 1) * self.ntimebins] = thishyper
 
-        getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
+        pipelinelog.getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
         # TODO add the wavelength WCS
 
     def makeHyper(self, file, applyweights=False, applymask=False, maxCountsCut=200):
@@ -320,30 +317,14 @@ class SpatialDrizzler(Drizzler):
         Drizzler.__init__(self, photonlists, metadata)
         self.driz = stdrizzle.Drizzle(outwcs=self.w, pixfrac=pixfrac)
 
-    def identify_hotpix(self, metadata, dithfrac=0.1, min_count=500, plot=True):
-        ndithers = len(metadata.dithHAs)
-        hot_cube = np.zeros((ndithers, metadata.ypix, metadata.xpix))
-        dith_cube = np.zeros_like(hot_cube)
-        for ix, file in enumerate(self.files):
-            dith_cube[ix], _ = self.makeImage(file)
-            # plt.imshow(dith_cube[ix], origin='lower')
-            # plt.show()
-        # hot_cube[dith_cube > min_count] = ma.masked
-        hot_cube[dith_cube > min_count] = 1
-        hot_amount_map = np.sum(hot_cube, axis=0)  # hot_cube.count(axis=0)
-        self.hot_mask = hot_amount_map / ndithers > dithfrac
-        if plot:
-            plt.imshow(self.hot_mask, origin='lower')
-            plt.show(block=True)
-
     def run(self, save_file=None, applymask=False):
         for ix, file in enumerate(self.files):
-            getLogger(__name__).debug('Processing %s', file)
+            pipelinelog.getLogger(__name__).debug('Processing %s', file)
             tic = time.clock()
-            insci, inwcs = self.makeImage(file)
+            insci, inwcs = self.makeImage(file, applymask=True)
             if applymask:
-                insci *= np.logical_not(self.hot_mask)
-            getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
+                insci *= ~self.hot_mask
+            pipelinelog.getLogger(__name__).debug('Image load done. Time taken (s): %s', time.clock() - tic)
             inwht = (insci != 0).astype(int)
             self.driz.add_image(insci, inwcs, inwht=inwht)
         if save_file:
@@ -362,12 +343,14 @@ class SpatialDrizzler(Drizzler):
                                                             normed=False)
 
         if applymask:
-            usablemask = np.rot90(file['usablemask']).astype(int)
-            # usablemask = file['usablemask'].T.astype(int)
+            pipelinelog.getLogger(__name__).debug("Applying bad pixel mask")
+            # usablemask = np.rot90(file['usablemask']).astype(int)
+            usablemask = file['usablemask'].T.astype(int)
             # thisImage *= ~usablemask
             thisImage *= usablemask
 
         if maxCountsCut:
+            pipelinelog.getLogger(__name__).debug("Applying max pixel count cut")
             thisImage *= thisImage < maxCountsCut
 
         w = wcs.WCS(naxis=2)
@@ -379,6 +362,31 @@ class SpatialDrizzler(Drizzler):
         w._naxis2 = self.ypix
         return thisImage, w
 
+    def get_persistant_bad(self, metadata, dithfrac=0.1, min_count=500, plot=True):
+        '''
+        Compare the same pixels at different dithers to determine if they are bad
+
+        :param metadata:
+        :param dithfrac:
+        :param min_count:
+        :param plot:
+        :return:
+
+        '''
+        ndithers = len(metadata.dithHAs)
+        hot_cube = np.zeros((ndithers, metadata.ypix, metadata.xpix))
+        dith_cube = np.zeros_like(hot_cube)
+        for ix, file in enumerate(self.files):
+            dith_cube[ix], _ = self.makeImage(file, applymask=False)
+            # plt.imshow(dith_cube[ix], origin='lower')
+            # plt.show()
+        # hot_cube[dith_cube > min_count] = ma.masked
+        hot_cube[dith_cube > min_count] = 1
+        hot_amount_map = np.sum(hot_cube, axis=0)  # hot_cube.count(axis=0)
+        self.hot_mask = hot_amount_map / ndithers > dithfrac
+        if plot:
+            plt.imshow(self.hot_mask, origin='lower')
+            plt.show(block=True)
 
 def get_wcs(x, y, ditherdesc, ditherind, nxpix=146, nypix=140, toa_rotation=False, randoffset=False, nPhot=1,
             platescale=0.01 / 3600.):
@@ -434,13 +442,9 @@ def get_wcs(x, y, ditherdesc, ditherind, nxpix=146, nypix=140, toa_rotation=Fals
         # undo the COR offset
         skyframe_photons = rotated_vgrid + ditherdesc.virPixCen
 
-    # Convert centroid positions in DD:MM:SS.S and HH:MM:SS.S format to radians.
-    centroidRADeg = ephem.hours(ditherdesc.cenRA).real
-    centroidDecDeg = ephem.degrees(ditherdesc.cenDec).real
-
     # Add the photon arcsecond offset to the centroid offset.
-    photDecDeg = centroidDecDeg + platescale * skyframe_photons[1]
-    photRADeg = centroidRADeg + platescale * skyframe_photons[0]
+    photDecDeg = ditherdesc.cenDec + platescale * skyframe_photons[1]
+    photRADeg = ditherdesc.cenRA + platescale * skyframe_photons[0]
 
     if randoffset:
         np.random.seed(42)  # so random values always same
@@ -524,18 +528,19 @@ def load_data(ditherdesc, wvlMin, wvlMax, startt, intt, tempfile='drizzler_tmp_{
 
             photons = obsfile.query(startw=startw, stopw=stopw, startt=startt, intt=intt)
             weights = photons['SpecWeight'] * photons['NoiseWeight']
-            getLogger(__name__).info("Fetched {} photons from {}".format(len(photons), file))
+            pipelinelog.getLogger(__name__).info("Fetched {} photons from {}".format(len(photons), file))
 
             x, y = obsfile.xy(photons)
             del obsfile
 
             q.put({'file': file, 'timestamps': photons["Time"], 'xPhotonPixels': x, 'yPhotonPixels': y,
                    'wavelengths': photons["Wavelength"], 'weight': weights, 'usablemask': usableMask})
-
-        getLogger(__name__).info('stacking number of dithers: %i'.format(ndither))
+            pipelinelog.getLogger(__name__).info('got to here')
+        pipelinelog.getLogger(__name__).info('stacking number of dithers: %i'.format(ndither))
 
         jobs = []
         data_q = mp.Queue()
+
         if ndither > 25:
             raise RuntimeError('Needs rewrite, will use too many cores')
 
@@ -545,17 +550,18 @@ def load_data(ditherdesc, wvlMin, wvlMax, startt, intt, tempfile='drizzler_tmp_{
             p.daemon = True
             p.start()
 
+        data = []
+        for t in range(ndither):
+            data.append(data_q.get())
+
         # Wait for all of the processes to finish fetching their data, this should hang until all the data has been
         # fetched
         for j in jobs:
             j.join()
 
-        data = []
-        for t in range(ndither):
-            data.append(data_q.get())
         data.sort(key=lambda k: filenames.index(k['file']))
 
-        getLogger(__name__).debug('Time spent: %f' % (time.time() - begin))
+        pipelinelog.getLogger(__name__).debug('Time spent: %f' % (time.time() - begin))
 
         with open(pkl_save, 'wb') as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -563,14 +569,14 @@ def load_data(ditherdesc, wvlMin, wvlMax, startt, intt, tempfile='drizzler_tmp_{
     # Do the dither
     for i, d in enumerate(data):
         radec = get_wcs(d['xPhotonPixels'], d['yPhotonPixels'], ditherdesc, i,
-                        nxpix=ditherdesc.xpix.ncols, nypix=ditherdesc.ypix, toa_rotation=False,
+                        nxpix=ditherdesc.xpix, nypix=ditherdesc.ypix, toa_rotation=False,
                         randoffset=False, nPhot=1, platescale=ditherdesc.platescale)
         d['photRARad'], d['photDecRad'] = radec
 
     return data
 
 
-def form(dither, dim=2, rotrate_mult=1, target='', wvlMin=850, wvlMax=1100, startt=0, intt=60, pixfrac=.5):
+def form(dither, dim='spatial', derotate=True, target='', wvlMin=850, wvlMax=1100, startt=0, intt=60, pixfrac=.5):
     """
 
     :param dim: 2->image, 3->spectral cube, 4->sequence of spectral cubes. If drizzle==False then dim is ignored
@@ -583,30 +589,30 @@ def form(dither, dim=2, rotrate_mult=1, target='', wvlMin=850, wvlMax=1100, star
     :param startt:
     :param intt:
     :param pixfrac:
-    :param drizzle bool for output format
+    :param drizzle: Bool for output format
 
     :return:
     """
 
     target = dither.name if not target else target
-    ditherdesc = DitherDescription(dither, rot_rate_multiplier=rotrate_mult, target=target)
+    ditherdesc = DitherDescription(dither, derotate=derotate, target=target)
     data = load_data(ditherdesc, wvlMin, wvlMax, startt, intt)
 
-    if dim not in range(2, 5):
-        raise ValueError('dim must be either 2, 3 or 4')
+    if dim not in ['spatial', 'spectral', 'temporal']:
+        raise ValueError('Not calling one of the available functions')
 
-    elif dim == 2:
+    elif dim == 'spatial':
         driz = SpatialDrizzler(data, ditherdesc, pixfrac=pixfrac)
-        # driz.identify_hotpix(ditherdesc)
-        driz.run(applymask=False)
+        driz.get_persistant_bad(ditherdesc)
+        driz.run(applymask=True)
         outsci = driz.driz.outsci
         outwcs = driz.w
 
-    elif dim == 3:
+    elif dim == 'spectral':
         # TODO implement, is this even necessary. On hold till interface specification and dataproduct definition
         raise NotImplementedError
 
-    elif dim == 4:
+    elif dim == 'temporal':
         tdriz = TemporalDrizzler(data, ditherdesc, pixfrac=pixfrac, nwvlbins=1, timestep=1.,
                                  wvlMin=wvlMin, wvlMax=wvlMax, startt=startt, intt=intt)
         tdriz.run()
@@ -633,7 +639,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # set up logging
-    mkidpipeline.logtoconsole()
+    # mkidpipeline.logtoconsole()
+    log_format = "%(levelname)s : %(message)s"
+    pipelinelog.create_log('mkidpipeline', console=True, fmt=log_format, level="INFO")
 
     # load as a task configuration
     cfg = mkidpipeline.config.load_task_config(args.cfg)
@@ -647,10 +655,10 @@ if __name__ == '__main__':
 
     image, drizwcs = form(dither, 'spatial', wvlMin=wvlMin, wvlMax=wvlMax, startt=startt, intt=intt, pixfrac=pixfrac)
 
-    pretty_plot(image, drizwcs.wcs.cdelt[0], drizwcs.wcs.crval, vmin=100, vmax=600)
-    write_fits(image, cfg.dither.target + '_mean.fits')
+    pretty_plot(image, drizwcs.wcs.cdelt[0], drizwcs.wcs.crval, vmin=5, vmax=600)
+    write_fits(image, cfg.dither.name + '_mean.fits')
 
-    tess, drizwcs = form(dither, 4, wvlMin=wvlMin, wvlMax=wvlMax, startt=startt, intt=intt, pixfrac=pixfrac)
+    tess, drizwcs = form(dither, 'temporal', wvlMin=wvlMin, wvlMax=wvlMax, startt=startt, intt=intt, pixfrac=pixfrac)
 
     y = np.ma.masked_where(tess[:, 0] == 0, tess[:, 0])
     medDither = np.ma.median(y, axis=0).filled(0)
