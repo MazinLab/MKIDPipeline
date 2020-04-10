840f2b874308744c28ed4e3d849ec0b39e00e16f
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-04-10 14:50:14 -0700

    Cleaned up code. Added commenting, removed untested portions and tested out remaining parts. Next step is adding functionality for plotting, removing cosmic ray photons for reporting/sanity checks, and adding diagnostic information

--- mkidpipeline/calibration/cosmics.py
+++ mkidpipeline/calibration/cosmics.py
@@ -5,51 +5,69 @@ to be removed plus some metadata, such as the cutout times before and after the 
 event, the method used for peak finding (more on that in the code itself), a set of data for
 creating a histogram of bincounts, and more as needed. This docstring will be edited as code
 is updated and refined.
+TODO: logging
+TODO: Add plotting/saving functionality
+TODO: Make this usable from the command line
+TODO: Test everything with real files (the 4 'paths' the data can take for CR removal)
+TODO: Integrate into pipeline
 """
 
 import numpy as np
 from mkidpipeline.hdf.photontable import ObsFile
 import argparse
 from scipy.stats import poisson
+from scipy import signal
 
 MEC_LASER_CAL_WAVELENGTH_RANGE = [850, 1375]
 DARKNESS_LASER_CAL_WAVELENGTH_RANGE = [808, 1310]
 BF_LASER_CAL_WAVELENGTH_RANGE = DARKNESS_LASER_CAL_WAVELENGTH_RANGE
 
 class CosmicCleaner(object):
-    def __init__(self, file, instrument=None, wavelengthCut=False, removalRange=[-50,100]):
-        self.instrument = instrument if instrument is not None else "MEC"
+    def __init__(self, file, instrument=None, wavelengthCut=False, method="poisson", removalRange=[-50,100]):
+        self.instrument = str(instrument) if instrument is not None else "MEC"
         self.obs = ObsFile(file)
         self.wavelengthCut = wavelengthCut  # Boolean that encodes the decision of which removal method to use:
-                                            # peak finding (no cut) or poisson statistics of arrival times (cut)
+        # peak finding (no cut) or poisson statistics of arrival times (cut)
+        self.method = method
         self.removalRange = removalRange
         self.allphotons = None  # Master photon list from the ObsFile, should not be modified at any point
-        self.photons = None # The photon list used for the cosmic ray removal. Not modified from allphotons if no cut
+        self.photons = None  # The photon list used for the cosmic ray removal. Not modified from allphotons if no cut
         self.photonMask = None # The mask used to remove photons from the master list (e.g. wvl outside of allowed range)
         self.arrivaltimes = None  # Arrival times from photons in self.photons, taken from self.photons for convenience
         self.arraycounts = None  # Number of counts across the array for each time bin
         self.timebins = None  # Edges of the time bins (microseconds) used to bin photon arrival times and generate
-                              # a photon timestream for the file. This is a property rather than an immediate step for
-                              # eventual plotting tools and use between functions
+        # a photon timestream for the file. This is a property rather than an immediate step for
+        # eventual plotting tools and use between functions
         self.timestream = None  # A (2,N) array where N is the number of time bins and the elements are a timestamp and
-                                # the number of counts across the array in that bin
-        self.trimmedtimestream = None # Same as self.timestream, but with cosmic rays removed, again for plotting
+        # the number of counts across the array in that bin
+        self.trimmedtimestream = None  # Same as self.timestream, but with cosmic rays removed, again for plotting
         self.countsperbin = None  # The data which will form the x-axis in a histogram of frequency of counts per bin
-                                  # in the ObsFile photon timestream
+        # in the ObsFile photon timestream
         self.countoccurrences = None  # The 'y-values' of the counts per bin histogram. If using Poisson statistics,
-                                      # this will be used to calculate the PDF needed to get a threshold value
-        self.pdf = None # The 'y-values' of the Poisson PDF, if used
+        # this will be used to calculate the PDF needed to get a threshold value
+        self.countshistogram = None  # A container for the previous two attributes, for plotting/reporting ease.
+        self.pdf = None  # The 'y-values' of the Poisson PDF, if used
+        self.threshold = None  # The minimum number of counts per bin which will be able to be identified as a cosmic
+        # ray event. If self.method='peak-finding', not all bins with values greater than this will necessarily be
+        # marked as cosmic rays, although all bins with values greater than this should still be removed (due to their
+        # proximity to the cosmic ray event.
         self.cutouttimes = None  # A list of all the timestamps affected by a cosmic ray event. This is the ultimate
-                                 # information that is needed to clean the ObsFile cosmic rays and remove the
-                                 # offending photons.
+        # information that is needed to clean the ObsFile cosmic rays and remove the
+        # offending photons.
+
+    def run(self):
+        self.get_photon_list()
+        self.make_timestream()
+        self.make_count_histogram()
+        self.generate_poisson_pdf()
 
     def get_photon_list(self):
-        '''
+        """
         Reads the photon list from the ObsFile, also creates a mask if wavelengthCut=True and
         applies it. Modifies self.allphotons and self.photons. If wavelengthCut=False
         self.allphotons=self.photons. If wavelengthCut=True, all photons outside the laser calibrated
         wavelengths will be removed from self.photons, self.allphotons will remain unchanged.
-        '''
+        """
         self.allphotons = self.obs.photonTable.read()
         if self.wavelengthCut:
             # With the wavelength cut, this step creates a mask and applies it so that we only keep photons within the
@@ -57,85 +75,122 @@ class CosmicCleaner(object):
             # and therefore have the same range. Any future instruments can be added to this by adding a dictionary
             # with their info at the top of the file.
             if self.instrument.upper() == "MEC":
-                self.photonMask = (self.allphotons > MEC_LASER_CAL_WAVELENGTH_RANGE[0]) & \
-                                  (self.allphotons < MEC_LASER_CAL_WAVELENGTH_RANGE[1])
+                self.photonMask = (self.allphotons['Wavelength'] >= MEC_LASER_CAL_WAVELENGTH_RANGE[0]) & \
+                                  (self.allphotons['Wavelength'] <= MEC_LASER_CAL_WAVELENGTH_RANGE[1])
                 self.photons = self.allphotons[self.photonMask]
             elif self.instrument.upper() == "DARKNESS":
-                self.photonMask = (self.allphotons > DARKNESS_LASER_CAL_WAVELENGTH_RANGE[0]) & \
-                                  (self.allphotons < DARKNESS_LASER_CAL_WAVELENGTH_RANGE[1])
+                self.photonMask = (self.allphotons['Wavelength'] >= DARKNESS_LASER_CAL_WAVELENGTH_RANGE[0]) & \
+                                  (self.allphotons['Wavelength'] <= DARKNESS_LASER_CAL_WAVELENGTH_RANGE[1])
                 self.photons = self.allphotons[self.photonMask]
             elif (self.instrument.upper() == "BLACKFRIDGE") or (self.instrument.upper() == "BF"):
-                self.photonMask = (self.allphotons > BF_LASER_CAL_WAVELENGTH_RANGE[0]) & \
-                                  (self.allphotons < BF_LASER_CAL_WAVELENGTH_RANGE[1])
+                self.photonMask = (self.allphotons['Wavelength'] >= BF_LASER_CAL_WAVELENGTH_RANGE[0]) & \
+                                  (self.allphotons['Wavelength'] <= BF_LASER_CAL_WAVELENGTH_RANGE[1])
                 self.photons = self.allphotons[self.photonMask]
             else:
                 print(f"WARNING: {self.instrument} is not a recognized instrument. No cut applied")
         else:
-            # If there is no wavelength cut desired, that means the peak-finding algorithm will be used as opposed to
-            # leveraging the poisson statistics when 'extrapolated-wl' photons are removed.
             self.photons = self.allphotons
 
     def get_time_info(self):
-        '''
+        """
         Extracts the timestamps of all the remaining photons and generates the bin edges to make the
         photon timestream (counts over array vs. time)
-        '''
+        """
         self.arrivaltimes = self.photons['Time']
         self.timebins = np.arange(0, int(np.ceil(self.arrivaltimes.max() / 10) * 10) + 10, 10)
+        assert self.timebins[-1] >= self.arrivaltimes.max()
 
     def make_timestream(self):
-        '''
+        """
         Uses the numpy.histogram function to bin all of the photons into 10 microsecond bins. Then creates the
         self.timestream attribute so that we can plot the array counts vs. time. We chop off the last entry of the
         time bins in the timestream because that is the end of the final bin, and we are plotting these events based on
-        the start times of each bin. We could also plot the midpoint of each bin, but because the removal range is 15
-        times larger than the bin size, it ultimately won't matter.
-        '''
+        the start times of each bin. We could also plot the midpoint of each bin but because the removal range is 15
+        times larger than the bin size it ultimately doesn't matter.
+        """
+        self.get_time_info()
         self.arraycounts, timebins = np.histogram(self.arrivaltimes, self.timebins)
         assert np.setdiff1d(timebins, self.timebins).size == 0
-        self.timestream = np.array((self.arraycounts, self.timebins[:-1]))
+        self.timestream = np.array((self.timebins[:-1], self.arraycounts))
 
     def make_count_histogram(self):
-        '''
+        """
         This function will use the numpy.unique function on the self.arraycounts attribute to return 2 lists, the first
-        will be all of the different values that occur in the self.arraycounts array, the second will be the corresponding
-        number of times that each of those values appear. This will allow us to create a histogram for data visualization
-        as well as create the Poisson PDF to determine the cosmic ray threshold value.
-        '''
+        will be all of the different values that occur in the self.arraycounts array, the second will be the
+        corresponding number of times that each of those values appear. This will allow us to create a histogram for
+        data visualization as well as create the Poisson PDF to determine the cosmic ray threshold value.
+        """
         unique, counts = np.unique(self.arraycounts, return_counts=True)
-        self.countsperbin = unique
-        self.countoccurrences = counts
+        self.countsperbin = unique  # This is the x-axis of the histogram. It will be filled with all of the values
+        # that occur in the self.arraycounts attribute.
+        self.countoccurrences = counts  # The y-axis of the histogram. It will be filled with the correspongding number
+        # of times that each value in self.countsperbin occurs.
+        self.countshistogram = np.array((self.countsperbin, self.countoccurrences))
+
+    def generate_poisson_pdf(self):
+        """
+        This function generates a Poisson probability density function fit to the histogram of counts per bin.
+        Regardless of whether or not photon list has been cut for wavelength the distribution should be Poisson and
+        should help provide a reasonable value for the number of counts per bin that can be classified as a cosmic ray.
+        """
+        avgcounts = np.average(self.arraycounts)  # Takes the average number of counts over the array in 10-microsecond
+        # bins to be used in creating the Poisson PDF
+        pdf = poisson.pmf(self.countshistogram[0], avgcounts)  # Creates a Poisson PDF of the number of counts per time
+        # bin. This can be used to compare with wavelength cut or non-cut data, but more care must be taken with non-cut
+        # data if there is a low probability of no counts over the array.
+        mask = pdf < 1 / np.sum(self.arraycounts)  # Creates a mask where the probability of that number of countsperbin
+        # happening is less than 1-in-all the counts during the observation.
+        pdf[mask] = 1 / np.sum(self.arraycounts)  # Applies the 'low-probability mask' to the PDF for (1) plotting and
+        # (2) helping determine where the threshold for cosmic ray cuts should be made.
+        self.pdf = pdf
+
+    def _generate_poisson_threshold(self):
+        """
+        For the Poisson cosmic ray identification method generates the threshold of counts in a given bin which will be
+        considered a cosmic ray event. For the Poisson method, that is the lowest value where the probability of that
+        number of counts occurring is essentially less than expected from the data. It also sets a lower limit in the
+        case that the photon list was not filtered based on wavelength and the Poisson statistics are trending towards
+        being Gaussian (and in turn low count rates also have near 0 probability).
+        :return:
+        threshold - The number of photons/10-microsecond bin
+        """
+        thresholdmask = (self.pdf <= 1 / np.sum(self.arraycounts)) & (self.countshistogram[0] >= 2)
+        threshold = self.countshistogram[0][thresholdmask].min()
+        return threshold
+
+    def _generate_signal_threshold(self):
+        """
+        For the peak-finding cosmic ray identification generates the threshold of counts per bin which will be used to
+        find the cosmic ray events. For the peak finding method, that is a value which is >6 times higher than the
+        average number of counts per bin. This value was found to be appropriate empirically and has been tested
+        predominantly on data which was not wavelength-cut. However, it works on either filtered or non-filtered data
+        and will be used to find appropriate cosmic ray 'signals' in self.find_cutout_times.
+        :return:
+        threshold - The number of photons/10-microsecond bin
+        """
+        avgcounts = np.average(self.arraycounts)
+        threshold = np.ceil(6 * poisson.std(avgcounts, loc=0) + avgcounts)
+        return threshold
 
     def find_cutout_times(self):
-        '''
-        The meat of the cosmic ray cleaning code.
-        In the wavelength cut mode, a Poisson pdf is generated where the
-        expected value is the mean number of counts from the time stream from the observation (empirically, the number
-        of cosmic ray events in a given observation doesn't drastically change the value of the mean). This pdf
-        is used to find the threshold of counts across the array that we will define as a cosmic ray. Once the threshold
-        is found, the timestamps of bins with counts higher than the threshold are found and stored in a list. After
-        that list is generated the full list of timestamps to be removed is made and the trimmedtimesteam attribute is
-        saved, again for diagnostic/recordkeeping purposes.
-        In the wavelength not cut mode ...
-        TODO: Implement the original peak-finding algorithm from Clarissa's code
-        '''
-        if self.wavelengthCut:
-            pdf = poisson.pmf(self.countsperbin, np.average(self.arraycounts))
-            mask = pdf <= 1 / np.sum(self.arraycounts)
-            pdf[mask] = 1 / np.sum(self.arraycounts)
-            self.pdf = pdf
-            maxcounts = self.pdf[~mask].max()
-            cutmask = self.arraycounts >= maxcounts
+        """
+        This function will generate the threshold of counts in a given bin that we can consider to be a cosmic ray. In
+        doing this, it will also create a 'trimmed' timestream where the cosmic rays are removed. The self.cutouttimes
+        attribute will also be populated with all of the timestamps that should be removed in order to take cosmic rays
+        out from the timestream.
+        """
+        if self.method.lower() == "poisson":
+            self.threshold = self._generate_poisson_threshold()
+            cutmask = self.arraycounts >= self.threshold
+            cosmictimes = self.timebins[:-1][cutmask]
 
-            cosmicraytimes = self.timebins[:-1][cutmask]
-            cutouttimes = np.array([np.arange(i-self.removalrange[0],
-                                              i+self.removalrange[1], 1) for i in cosmicraytimes]).flatten()
-            cutouttimes = np.array(list(dict.fromkeys(cutouttimes)))
-            self.cutouttimes = cutouttimes
-
-            trimmask = np.in1d(self.timebins[:-1], cutouttimes)
-            trimmedcounts = self.arraycounts[trimmask]
-            trimmedtimes = self.timebins[:-1][trimmask]
-            self.trimmedtimestream = np.array((trimmedcounts,trimmedtimes))
+        elif self.method.lower() == "peak-finding":
+            self.threshold = self._generate_signal_threshold()
+            cosmictimes = signal.find_peaks(self.arraycounts, height=self.threshold, threshold=10, distance=30)[0] * 10
         else:
-            #Do Clarissa's method
+            print("Invalid method!!")
+
+        cutouttimes = np.array([np.arange(i - self.removalRange[0],
+                                          i + self.removalRange[1], 1) for i in cosmictimes]).flatten()
+        cutouttimes = np.array(list(dict.fromkeys(cutouttimes)))
+        self.cutouttimes = cutouttimes
