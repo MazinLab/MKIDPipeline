9cd9cf44d86e2422283333963ae4fc73c485b1c5
Jeb Bailey <baileyji@ucsb.edu>
2021-03-15 14:10:43 -0700

    Move init code to mkidpipeline.pipeline and build a dict of steps

---
+++ mkidpipeline/pipeline.py
@@ -0,0 +1,91 @@
+from importlib import import_module
+import pkgutil
+
+from mkidcore.config import getLogger
+import mkidpipeline
+import mkidpipeline.hdf.photontable
+import mkidpipeline.config as config
+import mkidpipeline.steps
+
+
+log = getLogger('mkidpipeline')
+
+PIPELINE_STEPS = {}
+for info in pkgutil.iter_modules(mkidpipeline.steps.__path__):
+    mod = import_module(f"mkidpipeline.steps.{info.name}")
+    globals()[info.name] = mod
+    PIPELINE_STEPS[info.name] = mod
+
+
+def wavecal_apply(o):
+    if o.wavecal is None:
+        getLogger(__name__).info('No wavecal to apply for {}'.format(o.h5))
+        return
+    try:
+        of = mkidpipeline.hdf.photontable.Photontable(o.h5, mode='a')
+        of.applyWaveCal(wavecal.load_solution(o.wavecal.path))
+        of.file.close()
+    except Exception as e:
+        getLogger(__name__).critical('Caught exception during run of {}'.format(o.h5), exc_info=True)
+
+
+def flatcal_apply(o):
+    if o.flatcal is None:
+        getLogger(__name__).info('No flatcal to apply for {}'.format(o.h5))
+        return
+    try:
+        of = mkidpipeline.hdf.photontable.Photontable(o.h5, mode='a')
+        cfg = mkidpipeline.config.config
+        of.applyFlatCal(o.flatcal.path, use_wavecal=cfg.flatcal.use_wavecal, startw=850, stopw=1375)
+        of.file.close()
+    except Exception as e:
+        getLogger(__name__).critical('Caught exception during run of {}'.format(o.h5), exc_info=True)
+
+
+def linearitycal_apply(o):
+    try:
+        of = mkidpipeline.hdf.photontable.Photontable(o, mode='a')
+        cfg = mkidpipeline.config.config
+        of.applyLinearitycal(dt=cfg.linearitycal.dt, tau=cfg.instrument.deadtime*1*10**6)
+        of.file.close()
+    except Exception as e:
+        getLogger(__name__).critical('Caught exception during run of {}'.format(o), exc_info=True)
+
+
+def batch_apply_metadata(dataset):
+    """Function associates things not known at hdf build time (e.g. that aren't in the bin files)"""
+    # Retrieve metadata database
+    metadata = config.load_observing_metadata()
+    # Associate metadata
+    for ob in dataset.all_observations:
+        o = mkidpipeline.hdf.photontable.Photontable(ob.h5, mode='w')
+        mdl = config.select_metadata_for_h5(ob, metadata)
+        o.attach_observing_metadata(mdl)
+        del o
+
+
+def batch_apply_wavecals(obs, ncpu=None):
+    wavecal.clear_solution_cache()
+    pool = mp.Pool(ncpu if ncpu is not None else config.n_cpus_available())
+    obs = {o.h5: o for o in obs if o.wavecal is not None}.values()  # filter so unique h5 files, not responsible for a mixed wavecal specs
+    pool.map(wavecal_apply, obs)
+    pool.close()
+
+
+def batch_apply_flatcals(obs, ncpu=None):
+    pool = mp.Pool(ncpu if ncpu is not None else config.n_cpus_available())
+    obs = {o.h5: o for o in obs if o.flatcal is not None}.values()  # filter so unique h5 files, not responsible for a mixed flatcal specs
+    pool.map(flatcal_apply, obs)
+    pool.close()
+
+
+def batch_maskhot(obs, ncpu=None):
+    pool = mp.Pool(ncpu if ncpu is not None else config.n_cpus_available())
+    pool.map(badpix.mask_hot_pixels, set([o.h5 for o in obs]))
+    pool.close()
+
+
+def batch_apply_linearitycal(obs, ncpu=None):
+    pool = mp.Pool(ncpu if ncpu is not None else config.n_cpus_available())
+    pool.map(linearitycal_apply, set([o.h5 for o in obs]))
+    pool.close()
