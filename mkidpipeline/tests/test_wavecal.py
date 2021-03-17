5a25f047e243819eeb7934d35773b9b037c382ee
Jeb Bailey <baileyji@ucsb.edu>
2021-03-16 19:09:29 -0700

    add wavecal test

---
+++ mkidpipeline/tests/test_wavecal.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+import mkidpipeline.config as config
+import mkidpipeline.pipeline as pipe
+import pkg_resources as pkg
+
+config.logtoconsole()
+pipe.getLogger('mkidpipeline.steps.wavecal').setLevel('DEBUG')
+
+config.configure_pipeline(pkg.resource_filename('mkidpipeline', 'pipe.yml'))
+paths = dict(data='/darkdata/ScienceData/Subaru/', out='/work/tmp/tests/out',
+             database='/work/tmp/tests/database', tmp='')
+config.update_paths(paths)
+config.make_paths()
+config.config.update('ncpu', 2)
+dataset = config.load_data_description(pkg.resource_filename('mkidpipeline', 'tests/test_data.yml'))
+
+pipe.bin2hdf.buildtables(dataset.timeranges, remake=False)
+pipe.wavecal.fetch(dataset.wavecals, verbose=True)
