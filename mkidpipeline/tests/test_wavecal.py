80e0e7724d20c7b35dd3b4d0719975801f5cae89
Jeb Bailey <baileyji@ucsb.edu>
2021-03-16 20:55:10 -0700

    tweak test for wavecal

--- mkidpipeline/tests/test_wavecal.py
+++ mkidpipeline/tests/test_wavecal.py
@@ -4,15 +4,15 @@ import mkidpipeline.pipeline as pipe
 import pkg_resources as pkg
 
 config.logtoconsole()
-pipe.getLogger('mkidpipeline.steps.wavecal').setLevel('DEBUG')
+pipe.getLogger('mkidpipeline.steps.wavecal').setLevel('INFO')
 
 config.configure_pipeline(pkg.resource_filename('mkidpipeline', 'pipe.yml'))
 paths = dict(data='/darkdata/ScienceData/Subaru/', out='/work/tmp/tests/out',
              database='/work/tmp/tests/database', tmp='')
 config.update_paths(paths)
 config.make_paths()
-config.config.update('ncpu', 2)
+config.config.update('ncpu', 10)
 dataset = config.load_data_description(pkg.resource_filename('mkidpipeline', 'tests/test_data.yml'))
 
 pipe.bin2hdf.buildtables(dataset.timeranges, remake=False)
-pipe.wavecal.fetch(dataset.wavecals, verbose=True)
+pipe.wavecal.fetch(dataset.wavecals, verbose=False)
