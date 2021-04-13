889db7644b48d28095c5fb1047844a0513d3c603
Jeb Bailey <baileyji@ucsb.edu>
2021-04-12 18:30:07 -0700

    automatically generate an output.yaml. switch from yml to yaml per standard

--- mkidpipeline/tests/test_config.py
+++ mkidpipeline/tests/test_config.py
@@ -6,12 +6,12 @@ config.log_to_console()
 
 data = pipe.generate_sample_data()
 config.dump_dataconfig(data)
-d = config.MKIDObservingDataset('data.yml')
+d = config.MKIDObservingDataset('data.yaml')
 
-pcfg = config.configure_pipeline('pipe.yml')
+pcfg = config.configure_pipeline('pipe.yaml')
 data = pipe.generate_sample_data()
 config.dump_dataconfig(data)
-d = config.MKIDObservingDataset('data.yml')
+d = config.MKIDObservingDataset('data.yaml')
 
 
 
@@ -19,3 +19,8 @@ list(d.all_observations)
 list(d.wavecalable)
 list(d.flatcalable)
 list(d.speccalable)
+
+sample_out = pipe.generate_sample_output()
+with open('out.yaml', 'w') as f:
+    config.yaml.dump(sample_out, f)
+config.MKIDOutputCollection('out.yaml')
