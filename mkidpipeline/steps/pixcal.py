17803f52a4dd4a9ea968e14ebea7a61642e88d45
Sarah <steiger@physics.ucsb.edu>
2021-06-03 09:31:30 -0700

    pixcal cleaning

--- mkidpipeline/steps/pixcal.py
+++ mkidpipeline/steps/pixcal.py
@@ -14,7 +14,6 @@ from mkidcore.pixelflags import FlagSet
 def _calc_stdev(x):
     return np.nanstd(x) * _stddev_bias_corr((~np.isnan(x)).sum())
 
-
 def _stddev_bias_corr(n):
     if n == 1:
         corr = 1.0
@@ -28,7 +27,7 @@ def _stddev_bias_corr(n):
 
 class StepConfig(mkidpipeline.config.BaseStepConfig):
     yaml_tag = u'!pixcal_cfg'
-    REQUIRED_KEYS = (('method', 'median', 'method to use cpscut|laplacian|median|threshold'),
+    REQUIRED_KEYS = (('method', 'median', 'method to use laplacian|median|threshold'),
                      ('step', 30, 'Time interval for methods that need one'),
                      ('use_weight', True, 'Use photon weights'))
 
@@ -40,65 +39,25 @@ FLAGS = FlagSet.define(('hot', 1, 'Hot pixel'),
                        ('cold', 2, 'Cold pixel'),
                        ('unstable', 3, 'Pixel is both hot at and at different times'))
 
-
-# TODO make nsigma_hot and nsigma_cold user specified parameters in the config file
-def threshold(image, fwhm=4, box_size=5, nsigma_hot=4.0, nsigma_cold=4.0, max_iter=10,
-              use_local_stdev=False, bkgd_percentile=50.0, dead_mask=None, min_background_sigma=.01):
+# TODO make n_sigma a user specified parameter in the config file
+def threshold(image, fwhm=4, box_size=5, n_sigma=4.0, max_iter=10):
     """
-    Robust!  NOTE:  This is a routine that was ported over from the ARCONS pipeline.
-    Finds the hot and dead pixels in a for a 2D input array.
-    Compares the ratio of flux in each pixel to the median of the flux in an
-    enclosing box. If the ratio is too high -- i.e. the flux is too tightly
-    distributed compared to a Gaussian PSF of the expected FWHM -- then the
-    pixel is flagged as HOT.
-
-    If the pixel has counts less than 0.01, then the pixel is flagged as DEAD
-
-    The HOT and DEAD masks are combined into a single BAD mask at the end
-
-    Required Input:
-    :param image:           A 2D image array of photon counts.
-
-    Other Input:
-    :param fwhm:               Scalar float. Estimated full-width-half-max of the PSF (in pixels).
-    :param box_size:           Scalar integer. Size box used for calculating median flux in the region surrounding each pixel.
-    :param nsigma_hot:         Scalar float. If the flux ratio for a pixel is (nsigma_hot x expected error)
-                                             above the max expected given the PSF FWHM, then flag it as hot.
-    :param max_iter:           Scalar integer. Maximum number of iterations allowed.
-    :param dead_threshold:     Scalar integer. If a dead_mask is not given a dead_mask is created with this value as the
-                                               threshold. Defaults to 0.
-    :param use_local_stdev:    Bool.  If True, use the local (robust) standard deviation within the
-                                      moving box for the sigma value in the hot pixel thresholding
-                                      instead of Poisson statistics. Mainly intended for situations where
-                                      you know there is no astrophysical source in the image (e.g. flatfields,
-                                      laser calibrations), where you can also set fwhm=np.inf
-    :param bkgd_percentile:    Scalar Integer.  Percentile level (in %) in image to use as an estimate of the background.
-                                                In an ideal world, this will be 50% (i.e., the median of the image).
-                                                For raw images, however, there is often a gradient across the field,
-                                                in which case it's sensible to use something lower than 50%.
-    :param dead_mask:         Integer array. The input dead pixel mask.
-    :param min_background_sigma: Scalar float. Minimum counts for the pixel to be flagged as DEAD
-
+    Compares the ratio of flux in each pixel to the median of the flux in an enclosing box. If the ratio is too high
+     -- i.e. the flux is too tightly distributed compared to a Gaussian PSF of the expected FWHM -- then the pixel is
+      flagged as HOT.
+    :param image: 2D image array of photons (in counts)
+    :param fwhm: estimated full-width-half-max of the PSF (in pixels)
+    :param box_size: in pixels
+    :param n_sigma: number of standard deviations above/below the expected value for which a pixel will be flagged as
+     'hot'/'cold'
+    :param max_iter: maximum number of iterations
+    :param use_local_stdev:
     :return:
     A dictionary containing the result and various diagnostics. Keys are:
-
-    'bad_mask': the main output. Contains a 2D array of integers of the same shape as the input image, where:
-            0 = Good pixel
-            1 = Hot pixel
-            2 = Cold Pixel
-            3 = Dead Pixel
-    'dead_mask': 2D array of Bools of the same shape as the input image, where:
-                True = Dead Pixel
-                False = Not Dead Pixel
-    'hot_mask': 2D array of Bools of the same shape as the input image, where:
-                True = Hot Pixel
-                False = Not Hot Pixel
-    'image': 2D array containing the input image
+    'hot': boolean mask of hot pixels
+    'cold': boolean mask of cold pixels
     'masked_image': The hot and dead pixel masked image
-    'median_filter_image': The median-filtered image
-    'max_ratio': 2D array - the maximum allowed ratio between pixel flux and the median-filtered image
-    'difference_image': the difference between the input image and an image representing the max allowed flux in each pixel.
-    'difference_image_error': the expected error in the difference calculation.
+    'input_image': original input image
     'num_iter': number of iterations performed.
     """
 
@@ -109,46 +68,30 @@ def threshold(image, fwhm=4, box_size=5, nsigma_hot=4.0, nsigma_cold=4.0, max_it
     gauss_array = fitting.gaussian_psf(fwhm, box_size)
     max_ratio = np.max(gauss_array) / np.median(gauss_array)
 
-    # Assume everything with 0 counts is a dead pixel, turn dead pixel values into NaNs
-    if dead_mask is None:
-        dead_mask = np.ma.masked_where(raw_image == 0, raw_image).mask
-    raw_image[dead_mask] = np.nan
+    # turn dead pixel values into NaNs
+    raw_image[raw_image==0] = np.nan
 
     # Initialise a mask for hot pixels (all False) for comparison on each iteration.
-    initial_hot_mask = np.zeros_like(raw_image, dtype=bool)
-    initial_cold_mask = np.zeros_like(raw_image, dtype=bool)
+    reference_hot_mask = np.zeros_like(raw_image, dtype=bool)
+    reference_cold_mask = np.zeros_like(raw_image, dtype=bool)
     hot_mask = np.zeros_like(raw_image, dtype=bool)
     cold_mask = np.zeros_like(raw_image, dtype=bool)
 
-    # Initialise some arrays with NaNs in case they don't get filled out during the iteration
-    median_filter_image = np.full_like(raw_image, np.nan)
-    difference_image = np.full_like(raw_image, np.nan)
-    difference_image_error = np.full_like(raw_image, np.nan)
-
-    # In the case that *all* the pixels
-    # are dead, return a bad_mask where all the pixels are flagged as DEAD
-    iteration = -1
     if raw_image[np.isfinite(raw_image)].sum() <= 0:
         getLogger(__name__).warning('Entire image consists of pixels with 0 counts')
-        bad_mask = cold_mask * FLAGS.flags['cold'].bit
-        hot_mask = np.zeros_like(bad_mask, dtype=bool)
-
+        cold_mask = np.ones_like(raw_image, dtype=bool)
+        iteration = -1
     else:
         for iteration in range(max_iter):
-            getLogger(__name__).info('Doing iteration: {}'.format(iteration))
-            # Remove all the NaNs in an image and calculate a median filtered image
-            # each pixel takes the median of itself and the surrounding box_size x box_size box.
+            getLogger(__name__).info('Performing iteration: {}'.format(iteration + 1))
             nan_fixed_image = smoothing.replace_nan(raw_image, mode='mean', box_size=box_size)
             assert np.all(np.isfinite(nan_fixed_image))
             median_filter_image = spfilters.median_filter(nan_fixed_image, box_size, mode='mirror')
 
-            overall_median = np.nanmedian(raw_image)
-            overall_bkgd = np.percentile(raw_image[~np.isnan(raw_image)], bkgd_percentile)
+            median_bkgd = np.nanmedian(raw_image)
 
             # Estimate the background std. dev.
-            standard_filter_image = smoothing.nearest_n_robust_sigma_filter(raw_image, n=box_size ** 2 - 1)
-            overall_bkgd_sigma = max(min_background_sigma, np.nanmedian(standard_filter_image))
-            standard_filter_image.clip(1, None, standard_filter_image)
+            std_filter_image = smoothing.nearest_n_robust_sigma_filter(raw_image, n=box_size ** 2 - 1)
 
             # Calculate difference between flux in each pixel and max_ratio * the median in the enclosing box.
             # Also calculate the error that would exist in a measurement of a pixel that *was* at the peak of a real PSF
@@ -158,48 +101,44 @@ def threshold(image, fwhm=4, box_size=5, nsigma_hot=4.0, nsigma_cold=4.0, max_it
             #        flux > max_ratio*median - background*(max_ratio-1)
             # If the threshold is *lower* than the background, then set it equal to the background level instead
             # (a pixel below the background level is unlikely to be hot!)
-            getLogger(__name__).debug('overall_median: {}'.format(overall_median))
-            getLogger(__name__).debug('overall_bkgd: {}'.format(overall_bkgd))
-            getLogger(__name__).debug('overall_bkgd_sigma: {}'.format(overall_bkgd_sigma))
-            getLogger(__name__).debug('max_ratio: {}'.format(max_ratio))
-            threshold = np.maximum((max_ratio * median_filter_image - (max_ratio - 1) * overall_bkgd), overall_bkgd)
-            difference_image = raw_image - threshold
+            # TODO move above to better documentation location
 
-            # Simple estimate, photon error in the max value allowed. Neglect errors in the median itself here.
-            # Add in quadrature imaginary photon noise in the expected threshold level and background random noise
-            if use_local_stdev is False:
-                difference_image_error = np.sqrt(threshold + overall_bkgd_sigma ** 2)
-            else:
-                # use the local (robust) standard deviation within the moving box
-                difference_image_error = standard_filter_image
+            threshold = np.maximum((max_ratio * median_filter_image - (max_ratio - 1) * median_bkgd), median_bkgd)
+            getLogger(__name__).debug('Hot Pixel Masking Parameters:'
+                                      'hot flux threshold is {}'
+                                      'median background is {}'.format(threshold, median_bkgd))
+            hot_difference_image = raw_image - threshold
+            cold_difference_image = raw_image - median_filter_image
 
-            # Any pixel that has a peak/median ratio more than nSigma above the maximum ratio should be flagged as hot:
-            # True = bad pixel; False = good pixel.
-            hot_mask = (difference_image > (nsigma_hot * difference_image_error)) | initial_hot_mask
-            cold_mask = (raw_image < (overall_bkgd/nsigma_cold)) | initial_cold_mask # TODO don't know if this is the best way to do this
-            # If no change between between this and the last iteration then stop iterating
-            if np.all(hot_mask == initial_hot_mask) and np.all(cold_mask == initial_cold_mask):
+            # Any pixel that has a peak/median ratio more than n_sigma above the maximum ratio should be flagged as hot
+            # Any pixel that has a value less than n_sigma below the median should be flagged as cold
+            hot_mask = (hot_difference_image > (n_sigma * std_filter_image)) | reference_hot_mask
+            cold_mask = (cold_difference_image < - (n_sigma * std_filter_image)) | reference_cold_mask
+
+            #If no change between between this and the last iteration then stop iterating
+            if np.all(hot_mask == reference_hot_mask) and np.all(cold_mask == reference_cold_mask):
                 break
 
-            # Otherwise update 'initial_hot_mask' and 'initial_cold_mask' and set all detected bad pixels
+            # Otherwise update 'reference_hot_mask' and 'reference_cold_mask' and set all detected bad pixels
             # to NaN for the next iteration
-            initial_cold_mask = np.copy(cold_mask)
-            initial_hot_mask = np.copy(hot_mask)
+            reference_cold_mask = np.copy(cold_mask)
+            reference_hot_mask = np.copy(hot_mask)
             raw_image[hot_mask] = np.nan
             raw_image[cold_mask] = np.nan
-
-        # Finished with loop, make sure a pixel is not simultaneously hot and cold
+            if iteration == max_iter:
+                getLogger(__name__).info('Reached max number of iterations ({}) - Increase max iterations to ensure'
+                                         ' all hot pixels are masked or check your data for excessive'
+                                         ' outliers'.format(max_iter))
+        # make sure a pixel is not simultaneously hot and cold
         assert ~(hot_mask & cold_mask).any()
 
-    return {'hot': hot_mask, 'cold': cold_mask, 'masked_image': raw_image, 'image': image,
-            'median_filter_image': median_filter_image, 'max_ratio': max_ratio, 'difference_image': difference_image,
-            'difference_image_error': difference_image_error, 'num_iter': iteration + 1}
+    return {'hot': hot_mask, 'cold': cold_mask, 'masked_image': raw_image, 'input_image': image,
+            'num_iter': iteration + 1}
 
-
-def median(image, box_size=5, nsigma_hot=4.0, max_iter=5):
+def median(image, box_size=5, n_sigma=4.0, max_iter=5):
     """
     Passes a box_size by box_size moving box over the entire array and checks if the pixel at the center of that window
-    has counts higher than the median plus nsigma_hot times the standard deviation of the pixels in that window
+    has counts higher than the median plus n_sigma times the standard deviation of the pixels in that window
 
     If the pixel has counts less than 0.01, then the pixel is flagged as DEAD
 
@@ -213,7 +152,7 @@ def median(image, box_size=5, nsigma_hot=4.0, max_iter=5):
 
     Other Input:
     :param box_size:           Scalar integer. Size box used for calculating median counts in the region surrounding each pixel.
-    :param nsigma_hot:         Scalar float. If the flux ratio for a pixel is nsigma_hot x standard deviation within the moving box
+    :param n_sigma:         Scalar float. If the flux ratio for a pixel is n_sigma x standard deviation within the moving box
                                              above the max expected given the PSF FWHM, then flag it as hot.
     :param max_iter:           Scalar integer. Maximum number of iterations allowed.
 
@@ -270,9 +209,9 @@ def median(image, box_size=5, nsigma_hot=4.0, max_iter=5):
             func = lambda x: np.nanstd(x) * _stddev_bias_corr((~np.isnan(x)).sum())
             standard_filter_image = spfilters.generic_filter(nan_fixed_image, func, box_size, mode='mirror')
 
-            threshold = median_filter_image + (nsigma_hot * standard_filter_image)
+            threshold = median_filter_image + (n_sigma * standard_filter_image)
 
-            # Any pixel that has a count level more than nSigma above the median should be flagged as hot:
+            # Any pixel that has a count level more than n_sigma above the median should be flagged as hot:
             # True = bad pixel; False = good pixel.
             hot_mask = (median_filter_image > threshold) | initial_hot_mask
 
@@ -291,14 +230,14 @@ def median(image, box_size=5, nsigma_hot=4.0, max_iter=5):
             'num_iter': iteration + 1}
 
 
-def laplacian(image, box_size=5, nsigma_hot=4.0):
+def laplacian(image, box_size=5, n_sigma=4.0):
     """
     Required Input:
     :param image:           A 2D image array of photon counts.
 
     Other Input:
     :param box_size:           Scalar integer. Size box used for replacing the NaNs in the region surrounding each dead or NaN pixel.
-    :param nsigma_hot:         Scalar float. If the flux ratio for a pixel is (nsigma_hot x expected error)
+    :param n_sigma:         Scalar float. If the flux ratio for a pixel is (n_sigma x expected error)
                                              above the max expected given the PSF FWHM, then flag it as hot.
     :return:
     A dictionary containing the result and various diagnostics. Keys are:
@@ -339,7 +278,7 @@ def laplacian(image, box_size=5, nsigma_hot=4.0):
         dead_mask = np.ones_like(bad_mask, dtype=bool)
     else:
         laplacian_filter_image = spfilters.laplace(nan_fixed_image)
-        threshold_laplace = -(np.std(laplacian_filter_image) + nsigma_hot * np.std(laplacian_filter_image))
+        threshold_laplace = -(np.std(laplacian_filter_image) + n_sigma * np.std(laplacian_filter_image))
         hot_pix = np.where(laplacian_filter_image < threshold_laplace)
         hot_pix_x = hot_pix[0]
         hot_pix_y = hot_pix[1]
