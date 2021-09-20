73417e01cd233beef9244d0d0bde024737587ce1
Sarah <steiger@physics.ucsb.edu>
2021-09-20 16:15:21 -0700

    wcscal work

--- mkidpipeline/steps/wcscal.py
+++ mkidpipeline/steps/wcscal.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mkidpipeline.utils.smoothing import astropy_convolve
 from mkidpipeline.utils.photometry import fit_sources
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+import mkidpipeline.config as config
+from mkidpipeline.photontable import Photontable
+from mkidpipeline.steps.drizzler import form
+from astropy import wcs
 
 class StepConfig(mkidpipeline.config.BaseStepConfig):
     yaml_tag = u'!wcscal_cfg'
@@ -10,7 +16,7 @@ class StepConfig(mkidpipeline.config.BaseStepConfig):
                      ('interpolate', 'True', 'whether to inerpolate the image before PSF fitting. Recommended if an '
                                              'MKIDObservation is used or data is noisy'),
                      ('sigma_psf', 2.0, 'standard deviation of the point spread functions to fit in the image '),
-                     ('source_loc', [], 'locations of the sources in the image in (RA, DEC)'))
+                     ('frame', 'icrs', 'same as SkyCoord frame kwarg'))
 
 
 HEADER_KEYS = tuple()
@@ -41,7 +47,7 @@ class ClickCoords:
         self.push_to_start()
         plt.clf()
         plt.imshow(self.image)
-        plt.title(f'Select Locations of Source at {self.source_locs[self.counter]}')
+        plt.title(f'Select Location of Source at {self.source_locs[self.counter]}')
         plt.draw()
         self.cid = self.fig.canvas.mpl_connect('button_press_event', self.__onclick__)
         plt.show()
@@ -61,6 +67,7 @@ class ClickCoords:
         plt.draw()
         return self.coords
 
+
 def crop_image(image): #TODO move to utils
     """
     crops out buffer of pixels with 0 counts from an image
@@ -75,70 +82,79 @@ def crop_image(image): #TODO move to utils
     use_y = int(max(center_y - min_y, max_y - center_y))
     return image[center_x - use_x:center_x + use_x, center_y - use_y:center_y + use_y]
 
+
 def select_sources(image, source_locs):
     cc = ClickCoords(image, source_locs=source_locs)
     coords = cc.get_coords()
     return coords
+
 
 def closest_node(node, nodes):
     nodes = np.asarray(nodes)
     dist_2 = np.sum((nodes - node)**2, axis=1)
     return np.argmin(dist_2)
 
+
 def difference(a, b):
     diff = np.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
     return diff
+
 
 def angle(a, b):
     theta = np.arctan((a[0] - b[0])/(a[1] - b[1]))
     return theta
 
-def dict_dist_compare(coord_dict):
+
+def dist_compare(coord_dict, frame='icrs'):
     result = []
     for key1, val1 in coord_dict.items():
         for key2, val2 in coord_dict.items():
             if key1==key2:
                 pass
             else:
-                result.append(difference(key1, key2)/difference(val1, val2))
+                print(coord_dict)
+                print('KEYS ARE' + str(key1[0]) +',' + str(key1[1]))
+                coord1 = SkyCoord(key1[0], key1[1], frame=frame)
+                coord2 = SkyCoord(key2[0], key2[1], frame=frame)
+                result.append(coord1.separation(coord2).to(u.arcsecond).value/difference(val1, val2))
         return result
 
-def dict_theta_compare(coord_dict):
+
+def theta_compare(coord_dict, frame='icrs'):
     result = []
     for key1, val1 in coord_dict.items():
         for key2, val2 in coord_dict.items():
             if key1==key2:
                 pass
             else:
-                true_theta = angle(key1, key2)
-                measured_theta = angle(val1, val2)
+                coord1 = SkyCoord(key1[0], key1[1], frame=frame)
+                coord2 = SkyCoord(key2[0], key2[1], frame=frame)
+                true_theta = coord1.position_angle(coord2).to(u.deg).value
+                measured_theta = np.rad2deg(angle(val1, val2))
                 result.append(true_theta - measured_theta)
         return result
 
-def get_platescale(coord_dict):
+
+def get_platescale(coord_dict, frame='icrs'):
     pltscl = []
     dict_copy = coord_dict.copy()
     for key, value in coord_dict.items():
-        pltscl += dict_dist_compare(dict_copy)
+        pltscl += dist_compare(dict_copy, frame=frame)
         dict_copy.pop(key)
     return np.mean(pltscl) # TODO errors
 
-def get_rotation_angle(coord_dict):
+
+def get_rotation_angle(coord_dict, frame='icrs'):
     angles = []
     dict_copy = coord_dict.copy()
     for key, value in coord_dict.items():
-        angles += dict_theta_compare(dict_copy)
+        angles += theta_compare(dict_copy, frame=frame)
         dict_copy.pop(key)
     return np.mean(angles) # TODO errors
 
-def resolve_units(coords):
-    # TODO write
-    return coords
 
-def calculate_wcs_solution(image, source_locs=None, sigma_psf=2.0, convolve=False):
-    #TODO get source_locs in consistent units no matter what is given by user
-    source_locs = resolve_units(source_locs)
-    if convolve:
+def calculate_wcs_solution(image, source_locs=None, sigma_psf=2.0, interpolate=False, frame='icrs'):
+    if interpolate:
         im = astropy_convolve(image)
     else:
         im = image
@@ -152,10 +168,38 @@ def calculate_wcs_solution(image, source_locs=None, sigma_psf=2.0, convolve=Fals
     use_coord = [fit_coords[idx] for idx in use_idxs]
     for i, key in enumerate(coord_dict.keys()):
         coord_dict[key] = use_coord[i]
-    pltscl = get_platescale(coord_dict)
-    rot_angle = get_rotation_angle(coord_dict)
+    pltscl = get_platescale(coord_dict, frame=frame)
+    rot_angle = get_rotation_angle(coord_dict, frame=frame)
+
     return pltscl, rot_angle
 
 
-def fetch(data):
-    pass
+def run_wcscal(data, source_locs, sigma_psf=None, wave_start=950, wave_stop=1375, ncpu=None, interpolate=True, frame='icrs'):
+    if isinstance(data, config.MKIDDitherDescription):
+        drizzled = form(data, mode='drizzle', wave_start=wave_start, wave_stop=wave_stop, pixfrac=0.5,
+                        wcs_timestep=1, exclude_flags=PROBLEM_FLAGS, usecache=False,
+                        duration=min([o.duration for o in data.obs]), ncpu=ncpu, derotate=True)
+        image = drizzled.counts
+    else:
+        hdul = Photontable(data.h5).get_fits()
+        image = hdul[1].data
+    source_locs = [(s[0], s[1]) for s in source_locs]
+    platescale, rotation_angle = calculate_wcs_solution(image, source_locs, sigma_psf=sigma_psf, interpolate=interpolate,
+                                                        frame=frame)
+    return platescale, rotation_angle
+
+
+def fetch(solution_descriptors, config=None, ncpu=None):
+    try:
+        solution_descriptors = solution_descriptors.wcscals
+    except AttributeError:
+        pass
+
+    wcscfg = mkidpipeline.config.PipelineConfigFactory(step_defaults=dict(wcscal=StepConfig()), cfg=config, ncpu=ncpu,
+                                                       copy=True)
+    for sd in set(sd for sd in solution_descriptors):
+        print(sd.source_locs)
+        pltscl, rot_ang = run_wcscal(sd.data, sd.source_locs, sigma_psf=wcscfg.wcscal.sigma_psf, wave_start=950, wave_stop=1375,
+                                     ncpu=ncpu if ncpu is not None else 1, interpolate=wcscfg.wcscal.interpolate,
+                                     frame=wcscfg.wcscal.frame)
+        print(pltscl, rot_ang)
