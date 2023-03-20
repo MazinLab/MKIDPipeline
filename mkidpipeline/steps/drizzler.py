1bcad8835b4d077667346a0630527285979cb98a
Sarah <steiger@physics.ucsb.edu>
2023-03-20 13:35:49 -0700

    change np.round to math.isclose() for determining if wcs parameters are close enough in drizzler to not cause concern. In particular, np.round was causing undesired behavior when the last significant figure was a 5

--- mkidpipeline/steps/drizzler.py
+++ mkidpipeline/steps/drizzler.py
@@ -24,6 +24,7 @@ import pickle
 import hashlib
 from glob import glob
 import getpass
+from math import isclose
 from mkidcore.metadata import MetadataSeries
 import astropy
 from astropy.io import fits
@@ -388,7 +389,7 @@ class Drizzler(Canvas):
                     break
                 wcs_sol.pixel_shape = self.shape
                 # the sky grid ref and dither ref should match (crpix varies between dithers)
-                if not np.all(np.round(wcs_sol.wcs.crval, decimals=4) == np.round(self.wcs.wcs.crval, decimals=4)):
+                if not isclose(wcs_sol.wcs.crval, self.wcs.wcs.crval, rel_tol=1e-4):
                     getLogger(__name__).critical('sky grid ref and dither ref do not match '
                                                  '(crval varies between dithers)!')
                     raise RuntimeError('sky grid ref and dither ref do not match (crval varies between dithers)!')
