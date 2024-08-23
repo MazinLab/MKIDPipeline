6f5d40b537b1cdf6652c3eecef8a3afcee3d744e
Aled Cuda <aled@ucsb.edu>
2024-08-22 23:38:48 -0700

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -6,7 +6,7 @@ class TestMKIDPipe(TestCase):
         import mkidpipeline.mkidpipe as mkidpipe
         import os
         os.chdir("./tests/data/hip109427-subset")
-        mkidpipe.main(mkidpipe.parser().parse_args(args=["--make-dir", "--make-outputs"]))
+        self.assertEqual(mkidpipe.main(mkidpipe.parser().parse_args(args=["--make-dir", "--make-outputs"])), 0)
 
 if __name__ == "__main__":
     unittest.main()
